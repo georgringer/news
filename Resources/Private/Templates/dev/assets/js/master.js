c8f791742bef41ebb75c8de9ab622add8f86da99
Georg Ringer <mail@ringerge.org>
2010-10-19 10:56:59 +0000

    template from georg paul
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/news2/trunk@39326 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Resources/Private/Templates/dev/assets/js/master.js
@@ -0,0 +1,17 @@
+/*-----------------------------------------
+ 
+ copyright 2010 by opendo
+ 
+ 
+ Author: Georg Paul
+ 
+ Author URI: http://www.opendo.at
+ 
+ -------------------------------------------*/
+
+
+$(document).ready(function(){
+
+
+});
+
