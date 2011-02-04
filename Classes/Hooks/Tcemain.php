72d5699bc6882407c3d986500d53e4d8042f3264
just2b <just2b@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-04 17:45:50 +0000

    [FEATURE+] Preview of news records. #12345
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/news2/trunk@43105 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/Hooks/Tcemain.php
@@ -0,0 +1,72 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Georg Ringer <typo3@ringerge.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Hook into tcemain which is used to show preview of news item
+ *
+ * @package TYPO3
+ * @subpackage tx_news2
+ */
+class tx_News2_Hooks_Tcemain{
+
+	/**
+	 * Generate a different preview link
+	 * 
+	 * @param string $status status
+	 * @param string $table tablename
+	 * @param integer $id id of the record
+	 * @param array $fieldArray fieldArray
+	 * @param t3lib_TCEmain $parentObject parent Object
+	 */
+	public function processDatamap_afterDatabaseOperations($status, $table, $id, array $fieldArray, t3lib_TCEmain $parentObject) {
+		if ($table == 'tx_news2_domain_model_news') {
+			
+				// direct preview
+			if (!is_numeric($id)) {
+				$id=  $parentObject->substNEWwithIDs[$id];
+			}
+			
+			if (isset($GLOBALS['_POST']['_savedokview_x']) && !$fieldArray['type'] && !$GLOBALS['BE_USER']->workspace) {
+					// if "savedokview" has been pressed and current article has "type" 0 (= normal news article)
+					// and the beUser works in the LIVE workspace open current record in single view
+				$pagesTSC = t3lib_BEfunc::getPagesTSconfig($GLOBALS['_POST']['popViewId']); // get page TSconfig
+				if ($pagesTSC['tx_news2.']['singlePid']) {
+					$GLOBALS['_POST']['popViewId_addParams'] = ($fieldArray['sys_language_uid'] > 0 ?
+						'&L=' . $fieldArray['sys_language_uid'] : 0) . '&no_cache=1 ' . 
+						'&tx_news2_pi1[controller]=News&tx_news2_pi1[action]=detail&tx_news2_pi1[news]=' . $id;
+
+					$GLOBALS['_POST']['popViewId'] = $pagesTSC['tx_news2.']['singlePid'];
+				}
+			}
+		}
+	}	
+	
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/news2/Classes/Hooks/Tcemain.php'])    {
+    include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/news2/Classes/Hooks/Tcemain.php']);
+}
+
+?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -19,9 +19,12 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	)
 );
 
-
 	// Page module hook
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info'][$_EXTKEY . '_pi1'][$_EXTKEY] =
 	'EXT:' . $_EXTKEY . '/Resources/Private/Backend/class.tx_' . $_EXTKEY . '_cms_layout.php:tx_' . $_EXTKEY . '_cms_layout->getExtensionSummary';
+
+
+$GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$_EXTKEY] =
+	'EXT:' . $_EXTKEY. '/Classes/Hooks/Tcemain.php:tx_News2_Hooks_Tcemain';
 
 ?>
