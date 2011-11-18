ca647af697b4ceb6c60835794eb589a6f2276244
Georg Ringer <mail@ringerge.org>
2011-11-18 14:11:38 +0100

    [BUGFIX] sql statements in ext_tables.sql need lowercase
    EM shows that an update is needed but this is wrong.
    anyway writing it lowercase fixes the problem
    
    Change-Id: I827a117f90f4d2085c3b255727ce979642810d2c
    Resolves: #31877

--- ext_tables.sql
+++ ext_tables.sql
@@ -257,11 +257,11 @@ CREATE TABLE tx_news_domain_model_news_tag_mm (
 #
 # Table structure for table cf_news_categorycache
 CREATE TABLE cf_news_categorycache (
-    id int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    id int(11) unsigned NOT NULL auto_increment,
     identifier varchar(250) DEFAULT '' NOT NULL,
-    crdate int(11) UNSIGNED DEFAULT '0' NOT NULL,
+    crdate int(11) unsigned DEFAULT '0' NOT NULL,
     content mediumblob,
-    lifetime int(11) UNSIGNED DEFAULT '0' NOT NULL,
+    lifetime int(11) unsigned DEFAULT '0' NOT NULL,
     PRIMARY KEY (id),
     KEY cache_id (identifier)
 ) ENGINE=InnoDB;
@@ -270,7 +270,7 @@ CREATE TABLE cf_news_categorycache (
 # Table structure for table cf_news_categorycache_tags
 #
 CREATE TABLE cf_news_categorycache_tags (
-    id int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    id int(11) unsigned NOT NULL auto_increment,
     identifier varchar(250) DEFAULT '' NOT NULL,
     tag varchar(250) DEFAULT '' NOT NULL,
     PRIMARY KEY (id),
