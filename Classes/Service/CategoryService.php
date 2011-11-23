8ae9b95cee6a0926a0673b94f98e3b1575a15b40
Georg Ringer <mail@ringerge.org>
2011-11-23 20:35:51 +0100

    [FEATURE] Category rootline
    implement rootline up and not only children categories
    
    Change-Id: I8dcc930b015efebd2dc671afe78cb599bd15dc11
    Resolves: #32043

--- Classes/Service/CategoryService.php
+++ Classes/Service/CategoryService.php
@@ -41,11 +41,31 @@ class Tx_News_Service_CategoryService {
 	 */
 	public static function getChildrenCategories($idList, $counter = 0, $additionalWhere = '') {
 		$cache = t3lib_div::makeInstance('Tx_News_Service_CacheService', 'news_categorycache');
-		$cacheIdentifier = sha1($idList);
+		$cacheIdentifier = sha1('children' . $idList);
 
 		$entry = $cache->get($cacheIdentifier);
 		if(!$entry) {
 			$entry = self::getChildrenCategoriesRecursive($idList, $counter, $additionalWhere);
+			$cache->set($cacheIdentifier, $entry);
+		}
+		return $entry;
+	}
+
+	/**
+	 * Get rootline up by calling recursive function
+	 * and using the caching framework to save some queries
+	 *
+	 * @param integer $id category id to start
+	 * @param string $additionalWhere additional where clause
+	 * @return string comma seperated list of category ids
+	 */
+	public static function getRootline($id, $additionalWhere = '') {
+		$cache = t3lib_div::makeInstance('Tx_News_Service_CacheService', 'news_categorycache');
+		$cacheIdentifier = sha1('rootline' . $id);
+
+		$entry = $cache->get($cacheIdentifier);
+		if(!$entry) {
+			$entry = self::getRootlineRecursive($id, $additionalWhere);
 			$cache->set($cacheIdentifier, $entry);
 		}
 		return $entry;
@@ -82,6 +102,39 @@ class Tx_News_Service_CategoryService {
 			$result[] = $row['uid'] . ($subcategories ? ',' . $subcategories : '');
 		}
 		$GLOBALS['TYPO3_DB']->sql_free_result($res);
+
+		$result = implode(',', $result);
+		return $result;
+	}
+
+	/**
+	 * Get rootline categories
+	 *
+	 * @param integer $id category id to start
+	 * @param string $additionalWhere additional where clause
+	 * @return string comma seperated list of category ids
+	 */
+	public static function getRootlineRecursive($id, $counter=0, $additionalWhere = '') {
+		$id = (int)$id;
+		$result = array();
+
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+			'uid,parentcategory',
+			'tx_news_domain_model_category',
+			'uid=' . $id . ' AND deleted=0 ' . $additionalWhere);
+
+		$row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+		$GLOBALS['TYPO3_DB']->sql_free_result($res);
+		if ($id === 0 || !$row || $counter > 10000) {
+			$GLOBALS['TT']->setTSlogMessage('EXT:news: one or more recursive categories where found');
+			return $id;
+		}
+
+		$parent = self::getRootlineRecursive($row['parentcategory'], $counter, $additionalWhere);
+		$result[] = $row['parentcategory'];
+		if ($parent > 0) {
+			$result[] = $parent;
+		}
 
 		$result = implode(',', $result);
 		return $result;
