985d6f2df8279b565ca8227ee6d955f45bbb307f
Georg Ringer <mail@ringerge.org>
2011-12-05 20:22:22 +0100

    [BUGFIX] Add Utility_Compatibility to ext_autoload
    Missed the class in the autoloader
    
    Change-Id: Ia18a6d9d1cf2ac4696488551e2d8f33a18d34f72
    Resolves: #32308

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,6 +1,7 @@
 <?php
 $extensionClassesPath = t3lib_extMgm::extPath('news') . 'Classes/';
 return array(
+	'tx_news_utility_compatibility' => $extensionClassesPath . 'Utility/Compatibility.php',
 	'tx_news_utility_importjob' => $extensionClassesPath . 'Utility/ImportJob.php',
 	'tx_news_hooks_suggestreceiver' => $extensionClassesPath . 'Hooks/SuggestReceiver.php',
 	'tx_news_hooks_suggestreceivercall' => $extensionClassesPath . 'Hooks/SuggestReceiverCall.php',
