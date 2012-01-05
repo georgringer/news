2c14120ebe3fd806d3659ff631bd54a3119974d0
Georg Ringer <mail@ringerge.org>
2012-01-05 14:06:09 +0100

    [BUGFIX] Fix IncludeFileVH
    Change-Id: I94590be72d4d645b71b1b626857315ff9a336d7e

--- Classes/ViewHelpers/IncludeFileViewHelper.php
+++ Classes/ViewHelpers/IncludeFileViewHelper.php
@@ -40,9 +40,16 @@ class Tx_News_ViewHelpers_IncludeFileViewHelper extends Tx_Fluid_Core_ViewHelper
 	public function render($path, $compress = FALSE) {
 		if (TYPO3_MODE === 'FE') {
 			$path = $GLOBALS['TSFE']->tmpl->getFileName($path);
-		}
 
-		if ($path) {
+				// JS
+			if (strtolower(substr($path, -3)) === '.js') {
+				$GLOBALS['TSFE']->getPageRenderer()->addJsFile($path, NULL, $compress);
+
+				// CSS
+			} elseif (strtolower(substr($path, -4)) === '.css') {
+				$GLOBALS['TSFE']->getPageRenderer()->addCssFile($path, 'stylesheet', 'all', '', $compress);
+			}
+		} else {
 			$doc = t3lib_div::makeInstance('template');
 			$pageRenderer = $doc->getPageRenderer();
 
