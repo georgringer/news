97adb08269c8a9719a66e8d32def09ccab6c8412
Georg Ringer <mail@ringerge.org>
2012-01-27 18:13:24 +0100

    [TASK] Rename be module + other icont
    - Rename "News administration" to "News"
    - Change the icon to the one of the sysfolder
    
    Change-Id: I8b7afb7835c9bbc7c7e678543c33131774817df4

--- ext_tables.php
+++ ext_tables.php
@@ -334,7 +334,7 @@ if (TYPO3_MODE === 'BE') {
 		),
 		array(
 			'access' => 'user,group',
-			'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/import_module.gif',
+			'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/administration_module.png',
 			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_modadministration.xml',
 		)
 	);
