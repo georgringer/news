70c10b3221208d11d26d88b1adec696e46acb7a7
Georg Ringer <mail@ringerge.org>
2012-02-27 16:17:36 +0100

    [SECURITY] Check for valid ordering
    Fix valid ordering
    
    Change-Id: I9ace94848dfffa1381a01b6769a0cba2a591135c

---
+++ Classes/Utility/Validation.php
@@ -0,0 +1,75 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 Georg Ringer <typo3@ringerge.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Validation
+ *
+ * @package TYPO3
+ * @subpackage tx_news
+ */
+class Tx_News_Utility_Validation {
+
+	/**
+	 * Validate ordering as extbase can't handle that currently
+	 *
+	 * @param string $fieldToCheck
+	 * @param string $allowedSettings
+	 * @return boolean
+	 */
+	public static function isValidOrdering($fieldToCheck, $allowedSettings) {
+		$isValid = TRUE;
+
+		if (empty($fieldToCheck)) {
+			return $isValid;
+		} elseif(empty($allowedSettings)) {
+			return FALSE;
+		}
+
+		$fields = t3lib_div::trimExplode(',', $fieldToCheck, TRUE);
+		foreach($fields as $field) {
+			if ($isValid === TRUE) {
+				$split = t3lib_div::trimExplode(' ', $field, TRUE);
+				$count = count($split);
+				switch ($count) {
+					case 1:
+						if (!t3lib_div::inList($allowedSettings, $split[0])) {
+							$isValid = FALSE;
+						}
+						break;
+					case 2:
+						if ((strtolower($split[1]) !== 'desc' && strtolower($split[1]) !== 'asc') ||
+							!t3lib_div::inList($allowedSettings, $split[0])
+								) {
+							$isValid = FALSE;
+						}
+						break;
+					default:
+						$isValid = FALSE;
+				}
+			}
+		}
+
+		return $isValid;
+	}
+}
