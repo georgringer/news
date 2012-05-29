4c7fe6f493e44a90021537e6c5d59dec8140db19
Georg Ringer <mail@ringerge.org>
2012-05-29 19:20:16 +0200

    [FEATURE] Predefine archive date with TsConfig
    tx_news.predefine.archive in combination with strtotime
    makes it possible to set the archive date to e.g. next
    friday or always +2 weeks.
    
    Change-Id: Ic13fca376c23e9efedbcccf28066e56dde9dd376
    Resolves: #37281

---
+++ Classes/Hooks/Tceforms.php
@@ -0,0 +1,69 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 Georg Ringer <typo3@ringerge.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Hook into tceforms
+ *
+ * @package TYPO3
+ * @subpackage tx_news
+ */
+class Tx_News_Hooks_Tceforms {
+
+	/**
+	 * Preprocessing of fields
+	 *
+	 * @param string $table table name
+	 * @param string $field field name
+	 * @param array $row record row
+	 * @param string $altName
+	 * @param string $palette
+	 * @param string $extra
+	 * @param string $pal
+	 * @param t3lib_TCEforms $parentObject
+	 */
+	public function getSingleField_preProcess($table, $field, array &$row, $altName, $palette, $extra, $pal, t3lib_TCEforms $parentObject) {
+			// Predefine the archive date
+		if ($table === 'tx_news_domain_model_news' && !is_numeric($row['uid'])) {
+			$pagesTsConfig = t3lib_BEfunc::getPagesTSconfig($row['pid']);
+
+			if (is_array($pagesTsConfig['tx_news.']['predefine.'])
+					&& is_array($pagesTsConfig['tx_news.']['predefine.'])
+					&& isset($pagesTsConfig['tx_news.']['predefine.']['archive'])) {
+				$calculatedTime = strtotime($pagesTsConfig['tx_news.']['predefine.']['archive']);
+
+				if ($calculatedTime !== FALSE) {
+					$row['archive'] = $calculatedTime;
+				}
+			}
+		}
+	}
+
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/news/Classes/Hooks/Tceforms.php']) {
+	require_once ($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/news/Classes/Hooks/Tceforms.php']);
+}
+
+?>
