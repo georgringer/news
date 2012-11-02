2b32536849db503af9865057d3033c2f1b7dbc47
Georg Ringer <georg.ringer@gmail.com>
2012-11-02 17:55:21 +0100

    Allow recursion
    Change-Id: Ifc188d22442e7cf7e49fbbea3f6a635e78b9a517

--- Classes/TreeProvider/DatabaseTreeDataProvider.php
+++ Classes/TreeProvider/DatabaseTreeDataProvider.php
@@ -3,7 +3,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2010-2011 Steffen Ritter <info@steffen-ritter.net>
+ *  (c) 2012 Georg Ringer <typo3@ringerge.org>
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -26,16 +26,18 @@
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 /**
- * TCA tree data provider
+ * TCA tree data provider which considers
  *
- * @author Steffen Ritter <info@steffen-ritter.net>
+ * @author Georg Ringer <typo3@ringerge.org>
  * @package TYPO3
- * @subpackage t3lib_tree
+ * @subpackage tx_news
  */
 class Tx_News_TreeProvider_DatabaseTreeDataProvider extends \TYPO3\CMS\Core\Tree\TableConfiguration\DatabaseTreeDataProvider {
 
-	public function __construct($arguments) {
-
+	/**
+	 * @param array $configuration TCA configuration
+	 */
+	public function __construct(array $configuration) {
 	}
 
 	/**
@@ -46,7 +48,7 @@ class Tx_News_TreeProvider_DatabaseTreeDataProvider extends \TYPO3\CMS\Core\Tree
 	 * @param integer $level
 	 * @return An object
 	 */
-	protected function buildRepresentationForNode(\TYPO3\CMS\Backend\Tree\TreeNode $basicNode, \t3lib_tree_tca_DatabaseNode $parent = NULL, $level = 0) {
+	protected function buildRepresentationForNode(\TYPO3\CMS\Backend\Tree\TreeNode $basicNode, \t3lib_tree_tca_DatabaseNode $parent = NULL, $level = 0, $restriction = FALSE) {
 		$node = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('t3lib_tree_tca_DatabaseNode');
 		$row = array();
 		if ($basicNode->getId() == 0) {
@@ -57,14 +59,14 @@ class Tx_News_TreeProvider_DatabaseTreeDataProvider extends \TYPO3\CMS\Core\Tree
 			$row = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecordWSOL($this->tableName, $basicNode->getId(), '*', '', FALSE);
 
 
-				if ($this->getLabelField() !== '') {
-					$node->setLabel($row[$this->getLabelField()]);
-				} else {
-					$node->setLabel($basicNode->getId());
-				}
-				$node->setSelected(\TYPO3\CMS\Core\Utility\GeneralUtility::inList($this->getSelectedList(), $basicNode->getId()));
-				$node->setExpanded($this->isExpanded($basicNode));
-				$node->setLabel($node->getLabel() . '+ ' . $basicNode->getId());
+			if ($this->getLabelField() !== '') {
+				$node->setLabel($row[$this->getLabelField()]);
+			} else {
+				$node->setLabel($basicNode->getId());
+			}
+			$node->setSelected(\TYPO3\CMS\Core\Utility\GeneralUtility::inList($this->getSelectedList(), $basicNode->getId()));
+			$node->setExpanded($this->isExpanded($basicNode));
+			$node->setLabel($node->getLabel() . '+ ' . $basicNode->getId());
 		}
 
 		$node->setId($basicNode->getId());
@@ -76,9 +78,13 @@ class Tx_News_TreeProvider_DatabaseTreeDataProvider extends \TYPO3\CMS\Core\Tree
 			$node->setHasChildren(TRUE);
 			$childNodes = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Backend\\Tree\\SortedTreeNodeCollection');
 			foreach ($basicNode->getChildNodes() as $child) {
-				if( TYPO3\CMS\Core\Utility\GeneralUtility::inList($GLOBALS['BE_USER']->user['tx_news_categorymounts'], $child->getId())) {
-					$childNodes->append($this->buildRepresentationForNode($child, $node, $level + 1));
+
+				// Change in custom TreeDataProvider by adding the if clause
+				if($restriction || TYPO3\CMS\Core\Utility\GeneralUtility::inList($GLOBALS['BE_USER']->user['tx_news_categorymounts'], $child->getId())) {
+					$restriction = TRUE;
+					$childNodes->append($this->buildRepresentationForNode($child, $node, $level + 1, $restriction));
 				}
+				// Change in custom TreeDataProvider end
 			}
 			$node->setChildNodes($childNodes);
 		}
