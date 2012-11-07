cc4cb095ee2b1f03a517dd2429d4eec1d20b947d
Georg Ringer <georg.ringer@gmail.com>
2012-11-07 19:49:41 +0100

    [FEATURE] Allow disabling recursive category enabling
    When setting tx_news.singleCategoryAcl = 1 in
    UserTsConfig, *every* category record needs to
    be activated either in user or usergroup record.
    
    Change-Id: I8213ad71a0dc210fa3d51c1c332b16d2f7659c2d

--- Classes/TreeProvider/DatabaseTreeDataProvider.php
+++ Classes/TreeProvider/DatabaseTreeDataProvider.php
@@ -1,27 +1,21 @@
 <?php
 /***************************************************************
  *  Copyright notice
- *
  *  (c) 2012 Georg Ringer <typo3@ringerge.org>
  *  All rights reserved
- *
  *  This script is part of the TYPO3 project. The TYPO3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
- *
  *  The GNU General Public License can be found at
  *  http://www.gnu.org/copyleft/gpl.html.
  *  A copy is found in the textfile GPL.txt and important notices to the license
  *  from the author is found in LICENSE.txt distributed with these scripts.
- *
- *
  *  This script is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
@@ -35,7 +29,7 @@ class Tx_News_TreeProvider_DatabaseTreeDataProvider extends t3lib_tree_Tca_Datab
 	 *
 	 * @param array $configuration TCA configuration
 	 */
-	public function __construct(array $configuration) {
+	public function __construct (array $configuration) {
 	}
 
 	/**
@@ -46,44 +40,60 @@ class Tx_News_TreeProvider_DatabaseTreeDataProvider extends t3lib_tree_Tca_Datab
 	 * @param integer $level
 	 * @return t3lib_tree_tca_DatabaseNode node
 	 */
-	protected function buildRepresentationForNode(t3lib_tree_Node $basicNode, t3lib_tree_tca_DatabaseNode $parent = NULL, $level = 0, $restriction = FALSE) {
-		$node = t3lib_div::makeInstance('t3lib_tree_tca_DatabaseNode');
+	protected function buildRepresentationForNode (t3lib_tree_Node $basicNode, t3lib_tree_tca_DatabaseNode $parent = NULL, $level = 0, $restriction = FALSE) {
+		/**@param $node t3lib_tree_tca_DatabaseNode */
+		$node = t3lib_div::makeInstance ('t3lib_tree_tca_DatabaseNode');
 		$row = array();
-		if ($basicNode->getId() == 0) {
-			$node->setSelected(FALSE);
-			$node->setExpanded(TRUE);
-			$node->setLabel($GLOBALS['LANG']->sL($GLOBALS['TCA'][$this->tableName]['ctrl']['title']));
+		if ($basicNode->getId () == 0) {
+			$node->setSelected (FALSE);
+			$node->setExpanded (TRUE);
+			$node->setLabel ($GLOBALS['LANG']->sL ($GLOBALS['TCA'][$this->tableName]['ctrl']['title']));
 		} else {
-			$row = t3lib_BEfunc::getRecordWSOL($this->tableName, $basicNode->getId(), '*', '', FALSE);
+			$row = t3lib_BEfunc::getRecordWSOL ($this->tableName, $basicNode->getId (), '*', '', FALSE);
 
-			if ($this->getLabelField() !== '') {
-				$node->setLabel($row[$this->getLabelField()]);
+			if ($this->getLabelField () !== '') {
+				$node->setLabel ($row[$this->getLabelField ()]);
 			} else {
-				$node->setLabel($basicNode->getId());
+				$node->setLabel ($basicNode->getId ());
 			}
-			$node->setSelected(t3lib_div::inList($this->getSelectedList(), $basicNode->getId()));
-			$node->setExpanded($this->isExpanded($basicNode));
-			$node->setLabel($node->getLabel());
+			$node->setSelected (t3lib_div::inList ($this->getSelectedList (), $basicNode->getId ()));
+			$node->setExpanded ($this->isExpanded ($basicNode));
+			$node->setLabel ($node->getLabel ());
 		}
 
-		$node->setId($basicNode->getId());
-		$node->setSelectable(!t3lib_div::inList($this->getNonSelectableLevelList(), $level) && !in_array($basicNode->getId(), $this->getItemUnselectableList()));
-		$node->setSortValue($this->nodeSortValues[$basicNode->getId()]);
-		$node->setIcon(t3lib_iconWorks::mapRecordTypeToSpriteIconClass($this->tableName, $row));
-		$node->setParentNode($parent);
-		if ($basicNode->hasChildNodes()) {
-			$node->setHasChildren(TRUE);
-			$childNodes = t3lib_div::makeInstance('t3lib_tree_SortedNodeCollection');
-			foreach ($basicNode->getChildNodes() as $child) {
+		$node->setId ($basicNode->getId ());
+
+		// Break to force single category activation
+		if ($parent != NULL && $level != 0 && $this->isSingleCategoryAclActivated() && !$this->isCategoryAllowed ($node)) {
+			return NULL;
+		}
+		$node->setSelectable (!t3lib_div::inList ($this->getNonSelectableLevelList (), $level) && !in_array ($basicNode->getId (), $this->getItemUnselectableList ()));
+		$node->setSortValue ($this->nodeSortValues[$basicNode->getId ()]);
+		$node->setIcon (t3lib_iconWorks::mapRecordTypeToSpriteIconClass ($this->tableName, $row));
+		$node->setParentNode ($parent);
+		if ($basicNode->hasChildNodes ()) {
+			$node->setHasChildren (TRUE);
+			$childNodes = t3lib_div::makeInstance ('t3lib_tree_SortedNodeCollection');
+			$foundSomeChild = FALSE;
+			foreach ($basicNode->getChildNodes () as $child) {
 				// Change in custom TreeDataProvider by adding the if clause
-				if($restriction || $this->isCategoryAllowed($child)) {
-					$restriction = TRUE;
-					$childNodes->append($this->buildRepresentationForNode($child, $node, $level + 1, $restriction));
+				if ($restriction || $this->isCategoryAllowed ($child)) {
+					$returnedChild = $this->buildRepresentationForNode ($child, $node, $level + 1, TRUE);
+
+					if (!is_null ($returnedChild)) {
+						$foundSomeChild = TRUE;
+						$childNodes->append ($returnedChild);
+					} else {
+						$node->setParentNode (NULL);
+						$node->setHasChildren (FALSE);
+					}
 				}
-				$restriction = FALSE;
 				// Change in custom TreeDataProvider end
 			}
-			$node->setChildNodes($childNodes);
+//			$restriction = FALSE;
+			if ($foundSomeChild) {
+				$node->setChildNodes ($childNodes);
+			}
 		}
 		return $node;
 	}
@@ -94,13 +104,30 @@ class Tx_News_TreeProvider_DatabaseTreeDataProvider extends t3lib_tree_Tca_Datab
 	 * @param \TYPO3\CMS\Backend\Tree\TreeNode $child
 	 * @return bool
 	 */
-	protected function isCategoryAllowed($child) {
-		$mounts = Tx_News_Utility_CategoryProvider::getUserMounts();
+	protected function isCategoryAllowed ($child) {
+		$mounts = Tx_News_Utility_CategoryProvider::getUserMounts ();
 		if (empty($mounts)) {
 			return TRUE;
 		}
 
-		return t3lib_div::inList($mounts, $child->getId());
+		return t3lib_div::inList ($mounts, $child->getId ());
+	}
+
+	/**
+	 * By setting "tx_news.singleCategoryAcl = 1" in UserTsConfig
+	 * every category needs to be acticated, no recursive enabling
+	 *
+	 * @return bool
+	 */
+	protected function isSingleCategoryAclActivated() {
+		if (is_array($GLOBALS['BE_USER']->userTS['tx_news.'])
+			&& is_array($GLOBALS['BE_USER']->userTS['tx_news.']['category.'])
+			&& $GLOBALS['BE_USER']->userTS['tx_news.']['category.']['singleAcl'] === '1'
+		) {
+			return TRUE;
+		}
+
+		return FALSE;
 	}
 
 }
