839214dbcd5092a63057967856c1fd7ce6a6f5f5
Helmut Hummel <h.hummel@bitmotion.de>
2013-01-03 17:40:59 +0100

    [BUGFIX] Cast numberOfPages to integer
    Since ceil() always returns a float
    and a strict comparison is made later
    the check for current page fails in
    certain situations.
    
    Casting numberOfPages to integer solves
    this issue.
    
    Fixes: #44294
    Change-Id: Iebe9d5dfe0713f72fa1acb5d5a83e04e5a1c6b84
    Reviewed-on: https://review.typo3.org/17339
    Reviewed-by: Mattias Nilsson
    Reviewed-by: Stefan Neufeind
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

--- Classes/ViewHelpers/Widget/Controller/PaginateController.php
+++ Classes/ViewHelpers/Widget/Controller/PaginateController.php
@@ -86,7 +86,7 @@ class Tx_News_ViewHelpers_Widget_Controller_PaginateController extends Tx_Fluid_
 		$this->configuration = t3lib_div::array_merge_recursive_overrule(
 								$this->configuration,
 								(array)$this->widgetConfiguration['configuration'], TRUE);
-		$this->numberOfPages = ceil(count($this->objects) / (integer)$this->configuration['itemsPerPage']);
+		$this->numberOfPages = (integer)ceil(count($this->objects) / (integer)$this->configuration['itemsPerPage']);
 		$this->pagesBefore = (integer)$this->configuration['pagesBefore'];
 		$this->pagesAfter = (integer)$this->configuration['pagesAfter'];
 		$this->lessPages = (boolean)$this->configuration['lessPages'];
