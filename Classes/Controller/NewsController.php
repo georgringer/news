5741c93c07b29b63e1e1a9db876bf37e53085acb
Georg Ringer <georg.ringer@gmail.com>
2013-01-07 07:58:56 +0100

    [FEATURE] Extend DemandObjectFromSettings
    Add ->setMonth() and ->setYear()
    
    Change-Id: I39998fd4647016a401b68646a0613045db84b6fc
    Resolves: #37806
    Reviewed-on: https://review.typo3.org/17403
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

--- Classes/Controller/NewsController.php
+++ Classes/Controller/NewsController.php
@@ -91,6 +91,8 @@ class Tx_News_Controller_NewsController extends Tx_News_Controller_NewsBaseContr
 
 		$demand->setSearchFields($settings['search']['fields']);
 		$demand->setDateField($settings['dateField']);
+		$demand->setMonth($settings['month']);
+		$demand->setYear($settings['year']);
 
 		$demand->setStoragePage(Tx_News_Utility_Page::extendPidListByChildren($settings['startingpoint'],
 			$settings['recursive']));
