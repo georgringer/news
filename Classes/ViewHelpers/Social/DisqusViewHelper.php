94fffd95524ec71d111462151609e67340990918
Georg Ringer <georg.ringer@gmail.com>
2013-01-18 16:33:55 +0100

    [FEATURE] Support locales in disqus VH
    Change-Id: I602d3854ddc95e45dcf649040ad670acf5c33986
    Resolves: #44637

--- Classes/ViewHelpers/Social/DisqusViewHelper.php
+++ Classes/ViewHelpers/Social/DisqusViewHelper.php
@@ -1,37 +1,31 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2010 Georg Ringer <typo3@ringerge.org>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *  (c) 2010 Georg Ringer <typo3@ringerge.org>
+ *  All rights reserved
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * ViewHelper to add disqus thread
  * Details: http://www.disqus.com/
- *
  * Example
  * ==============
  * <div id="disqus_thread"></div>
  * <n:social.disqus newsItem="{newsItem}"
- * 		shortName="demo123"
- * 		link="{n:link(newsItem:newsItem,settings:settings,uriOnly:1)}" />
+ *         shortName="demo123"
+ *         link="{n:link(newsItem:newsItem,settings:settings,uriOnly:1)}" />
  *
  * @package TYPO3
  * @subpackage tx_news
@@ -39,6 +33,19 @@
 class Tx_News_ViewHelpers_Social_DisqusViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
 
 	protected $escapingInterceptorEnabled = FALSE;
+
+	/**
+	 * @var Tx_News_Service_SettingsService
+	 */
+	protected $pluginSettingsService;
+
+	/**
+	 * @var Tx_News_Service_SettingsService $pluginSettingsService
+	 * @return void
+	 */
+	public function injectSettingsService(Tx_News_Service_SettingsService $pluginSettingsService) {
+		$this->pluginSettingsService = $pluginSettingsService;
+	}
 
 	/**
 	 * Render disqus thread
@@ -49,11 +56,16 @@ class Tx_News_ViewHelpers_Social_DisqusViewHelper extends Tx_Fluid_Core_ViewHelp
 	 * @return string
 	 */
 	public function render(Tx_News_Domain_Model_News $newsItem, $shortName, $link) {
+		$tsSettings = $this->pluginSettingsService->getSettings();
+
 		$code = '<script type="text/javascript">
 					var disqus_shortname = ' . t3lib_div::quoteJSvalue($shortName, TRUE) . ';
 					var disqus_identifier = \'news_' . $newsItem->getUid() . '\';
 					var disqus_url = ' . t3lib_div::quoteJSvalue($link, TRUE) . ';
 					var disqus_title = ' . t3lib_div::quoteJSvalue($newsItem->getTitle(), TRUE) . ';
+					var disqus_config = function () {
+						this.language = ' . t3lib_div::quoteJSvalue($tsSettings['disqusLang']) . ';
+					};
 
 					(function() {
 						var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;
