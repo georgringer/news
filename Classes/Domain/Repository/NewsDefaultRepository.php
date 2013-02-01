beebaa434669639eb1bfc0956e31a32ac90d51a7
Georg Ringer <georg.ringer@gmail.com>
2013-02-01 18:26:58 +0100

    [TASK] Add a NewsDefaultRepository
    The NewsDefaultRepository is needed if a model is updated
    
    Change-Id: I7b402b1b8a8a89aa459aa5d3ba397f06d88b56dd
    Resolves: #45075

---
+++ Classes/Domain/Repository/NewsDefaultRepository.php
@@ -0,0 +1,35 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Georg Ringer <typo3@ringerge.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * News repository with all the callable functionality
+ *
+ * @package TYPO3
+ * @subpackage tx_news
+ */
+class Tx_News_Domain_Repository_NewsDefaultRepository extends Tx_News_Domain_Repository_NewsRepository  {
+
+}
+
+?>
