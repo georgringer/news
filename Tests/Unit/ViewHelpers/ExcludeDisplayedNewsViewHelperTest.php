1d7f42e5ce6a5fdb1b4345e29f987e9b592dbcd9
Georg Ringer <georg.ringer@gmail.com>
2013-02-05 00:02:20 +0100

    [TASK] Unittest for ExcludeDisplayedNewsVH
    Change-Id: I9ac5d9cea13038c5cc309c6c50636731ef983868
    Reviewed-on: https://review.typo3.org/18017
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

---
+++ Tests/Unit/ViewHelpers/ExcludeDisplayedNewsViewHelperTest.php
@@ -0,0 +1,58 @@
+<?php
+	/***************************************************************
+	 *  Copyright notice
+	 *
+	 *  (c) 2013 Georg Ringer <typo3@ringerge.org>
+	 *  All rights reserved
+	 *
+	 *  This script is part of the TYPO3 project. The TYPO3 project is
+	 *  free software; you can redistribute it and/or modify
+	 *  it under the terms of the GNU General Public License as published by
+	 *  the Free Software Foundation; either version 2 of the License, or
+	 *  (at your option) any later version.
+	 *
+	 *  The GNU General Public License can be found at
+	 *  http://www.gnu.org/copyleft/gpl.html.
+	 *
+	 *  This script is distributed in the hope that it will be useful,
+	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *  GNU General Public License for more details.
+	 *
+	 *  This copyright notice MUST APPEAR in all copies of the script!
+	 ***************************************************************/
+
+	/**
+	 * Tests for Tx_News_ViewHelpers_ExcludeDisplayedNewsViewHelper
+	 *
+	 * @package TYPO3
+	 * @subpackage tx_news
+	 * @author Georg Ringer <typo3@ringerge.org>
+	 */
+class Tx_News_Tests_ViewHelpers_ExcludeDisplayedNewsViewHelperTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
+
+	/**
+	 * @test
+	 * @return void
+	 */
+	public function newsIsAddedToExcludedList() {
+
+		$viewHelper = new Tx_News_ViewHelpers_ExcludeDisplayedNewsViewHelper();
+		$this->assertEquals($GLOBALS['EXT']['news']['alreadyDisplayed'], NULL);
+
+		$newsItem1 = new Tx_News_Domain_Model_News();
+		$newsItem1->_setProperty('uid','123');
+
+		$viewHelper->render($newsItem1);
+		$this->assertEquals($GLOBALS['EXT']['news']['alreadyDisplayed'], array('123' => '123'));
+
+		$newsItem1 = new Tx_News_Domain_Model_News();
+		$newsItem1->_setProperty('uid','123');
+		$this->assertEquals($GLOBALS['EXT']['news']['alreadyDisplayed'], array('123' => '123'));
+
+		$newsItem2 = new Tx_News_Domain_Model_News();
+		$newsItem2->_setProperty('uid','12');
+		$viewHelper->render($newsItem2);
+		$this->assertEquals($GLOBALS['EXT']['news']['alreadyDisplayed'], array('123' => '123', '12' => '12'));
+	}
+}
