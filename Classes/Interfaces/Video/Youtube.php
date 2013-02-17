2552ce787ce953eb8da9dc71ab99e45ec901b9dc
Georg Ringer <georg.ringer@gmail.com>
2013-02-17 13:56:05 +0100

    [FEATURE] Support for youtube videos
    Show youtube videos with an iframe
    
    Change-Id: Ia308a595bd61d9180ef23e33152e7c7dd0de95d7

---
+++ Classes/Interfaces/Video/Youtube.php
@@ -0,0 +1,92 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *  (c) 2013 Georg Ringer <typo3@ringerge.org>
+ *  All rights reserved
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Implementation of youtube support
+ *
+ * @package TYPO3
+ * @subpackage tx_news
+ */
+class Tx_News_Interfaces_Video_Youtube implements Tx_News_Interfaces_MediaInterface {
+
+	/**
+	 * Render videos from youtube
+	 *
+	 * @param Tx_News_Domain_Model_Media $element
+	 * @param integer $width
+	 * @param integer $height
+	 * @return string
+	 */
+	public function render(Tx_News_Domain_Model_Media $element, $width, $height) {
+		$content = '';
+
+		$url = $this->getYoutubeUrl($element);
+
+		if (!is_null($url)) {
+			// override width & height if both are set
+			if ($element->getWidth() > 0 && $element->getHeight() > 0) {
+				$width = $element->getWidth();
+				$height = $element->getHeight();
+			}
+
+			$content = '<iframe width="' . (int)$width . '" height="' . (int)$height . '" src="' . htmlspecialchars($url) . '" frameborder="0"></iframe>';
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Check if given element includes an url to a youtube video
+	 *
+	 * @param Tx_News_Domain_Model_Media $element
+	 * @return boolean
+	 */
+	public function enabled(Tx_News_Domain_Model_Media $element) {
+		$result = FALSE;
+
+		$url = $this->getYoutubeUrl($element);
+
+		if (!is_null($url)) {
+			$result = TRUE;
+		}
+		return $result;
+	}
+
+
+	/**
+	 * @param Tx_News_Domain_Model_Media $element
+	 * @return null|string
+	 */
+	public function getYoutubeUrl(Tx_News_Domain_Model_Media $element) {
+		$videoId = NULL;
+		$youtubeUrl = NULL;
+
+		if (preg_match('/(v=|v\\/|.be\\/)([^(\\&|$)]*)/', $element->getContent(), $matches)) {
+			$videoId = $matches[2];
+		}
+		if ($videoId) {
+			$youtubeUrl = 'http://www.youtube.com/embed/' . $videoId . '?fs=1';
+		}
+
+		return $youtubeUrl;
+	}
+
+}
+
+?>
---
+++ Tests/Unit/Interfaces/Video/YoutubeTest.php
@@ -0,0 +1,72 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2013 Georg Ringer <typo3@ringerge.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Tests for Tx_News_Interfaces_Video_Youtube
+ */
+class Tx_News_Tests_Interfaces_Video_YoutubeTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
+
+	/**
+	 * @test
+	 * @dataProvider fileIsRecognizedDataProvider
+	 * @return void
+	 */
+	public function flvFileIsRecognized($expected, $expectedOutput) {
+		$mediaElement = new Tx_News_Domain_Model_Media();
+		$mediaElement->setMultimedia($expected);
+		$mediaElement->setType(Tx_News_Domain_Model_Media::MEDIA_TYPE_MULTIMEDIA);
+
+		$renderer = new Tx_News_Interfaces_Video_Youtube();
+		$this->assertEquals($expectedOutput, $renderer->enabled($mediaElement));
+	}
+
+	/**
+	 * @return array
+	 */
+	public function fileIsRecognizedDataProvider() {
+		return array(
+			'defaultUrl' => array(
+				'http://www.youtube.com/watch?v=IGX2dXpTyns', TRUE
+			),
+			'shortUrl' => array(
+				'http://youtu.be/ko5CCSomDMY', TRUE
+			),
+			'noMediaFileGiven' => array(
+				NULL, FALSE
+			),
+			'emptyMediaFileGiven' => array(
+				'', FALSE
+			),
+			'localFileGiven' => array(
+				'fileadmin/fobar.flv', FALSE
+			),
+			'wrongDomainGiven' => array(
+				'http://www.somedomain.com/watch/1234', FALSE
+			),
+		);
+	}
+
+}
+
+?>
