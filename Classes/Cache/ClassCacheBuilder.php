a9a67babc907a19b767f08d0636c7176ccce68c5
Georg Ringer <georg.ringer@gmail.com>
2013-02-21 17:08:58 +0100

    [TASK] Improve ClassCacheBuilder + Tests
    Change-Id: Ia2dca96c1c2438631e03e4bc925689b5cc0af967

--- Classes/Cache/ClassCacheBuilder.php
+++ Classes/Cache/ClassCacheBuilder.php
@@ -1,26 +1,21 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2012 Georg Ringer <typo3@ringerge.org>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *  (c) 2012 Georg Ringer <typo3@ringerge.org>
+ *  All rights reserved
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * Class Cache builder
@@ -30,21 +25,63 @@
  */
 class Tx_News_Cache_ClassCacheBuilder {
 
+	const CACHE_FILE_LOCATION = 'typo3temp/Cache/Code/cache_phpcode/';
+
 	/**
 	 * Builds the proxy files
 	 *
 	 * @return array information for the autoloader
+	 * @throws Exception
 	 */
-	public static function build() {
+	public function build() {
+		$this->init();
 		$cacheEntries = array();
-		$t3libUtilityVersionNumber = PATH_t3lib . 'utility/class.t3lib_utility_versionnumber.php';
-		if (is_file($t3libUtilityVersionNumber)) {
-			require_once($t3libUtilityVersionNumber);
+
+		$extensibleExtensions = $this->getExtensibleExtensions();
+
+		foreach ($extensibleExtensions as $key => $extensionsWithThisClass) {
+			$extendingClassFound = FALSE;
+
+			// Get the file from news itself, this needs to be loaded as first
+			$path = t3lib_extMgm::extPath('news') . 'Classes/' . $key . '.php';
+			if (!is_file($path)) {
+				throw new Exception('given file "' . $path . '" does not exist');
+			}
+			$code = $this->parseSingleFile($path, FALSE);
+
+			// Get the files from all other extensions
+			foreach ($extensionsWithThisClass as $extension => $value) {
+				$path = t3lib_extMgm::extPath($extension) . 'Classes/' . $key . '.php';
+				if (is_file($path)) {
+					$extendingClassFound = TRUE;
+					$code .= $this->parseSingleFile($path);
+				}
+			}
+
+			// If an extending class is found, the file is written and
+			// added to the autoloader info
+			if ($extendingClassFound) {
+				$cacheIdentifier = 'tx_news_' . strtolower(str_replace('/', '_', $key));
+				try {
+					$cacheEntries[$cacheIdentifier] = $this->writeFile($code, $key);
+				} catch (Exception $e) {
+					throw new Exception($e->getMessage());
+				}
+			}
 		}
-		require_once(t3lib_extMgm::extPath('news', 'Classes/Utility/Compatibility.php'));
+
+		return $cacheEntries;
+	}
+
+	/**
+	 * Get all loaded extensions which try to extend EXT:news
+	 *
+	 * @return array
+	 */
+	protected function getExtensibleExtensions() {
 		$loadedExtensions = array_unique(t3lib_div::trimExplode(',', Tx_News_Utility_Compatibility::getLoadedExtensionList(), TRUE));
 
-			// Get the extensions which want to extend news
+		// Get the extensions which want to extend news
 		$extensibleExtensions = array();
 		foreach ($loadedExtensions as $extensionKey) {
 			$extensionInfoFile = t3lib_extMgm::extPath($extensionKey, 'Resources/Private/extend-news.txt');
@@ -56,39 +93,20 @@ class Tx_News_Cache_ClassCacheBuilder {
 				}
 			}
 		}
+		return $extensibleExtensions;
+	}
 
-		foreach ($extensibleExtensions as $key => $extensionsWithThisClass) {
-			$extendingClassFound = FALSE;
-
-				// Get the file from news itself, this needs to be loaded as first
-			$path = t3lib_extMgm::extPath('news') . 'Classes/' . $key . '.php';
-			if (!is_file($path)) {
-				throw new Exception('given file "' . $path . '" does not exist');
-			}
-			$code = self::parseSingleFile($path, FALSE);
-
-				// Get the files from all other extensions
-			foreach ($extensionsWithThisClass as $extension => $value) {
-				$path = t3lib_extMgm::extPath($extension) . 'Classes/' . $key . '.php';
-				if (is_file($path)) {
-					$extendingClassFound = TRUE;
-					$code .= self::parseSingleFile($path);
-				}
-			}
-
-				// If an extending class is found, the file is written and
-				// added to the autoloader info
-			if ($extendingClassFound) {
-				$cacheIdentifier = 'tx_news_' . strtolower(str_replace('/', '_', $key));
-				try {
-					$cacheEntries[$cacheIdentifier] = self::writeFile($code, $key);
-				} catch (Exception $e) {
-					throw new Exception($e->getMessage());
-				}
-			}
+	/**
+	 * Load some classes
+	 *
+	 * @return void
+	 */
+	protected function init() {
+		$t3libUtilityVersionNumber = PATH_t3lib . 'utility/class.t3lib_utility_versionnumber.php';
+		if (is_file($t3libUtilityVersionNumber)) {
+			require_once($t3libUtilityVersionNumber);
 		}
-
-		return $cacheEntries;
+		require_once(t3lib_extMgm::extPath('news', 'Classes/Utility/Compatibility.php'));
 	}
 
 	/**
@@ -98,20 +116,38 @@ class Tx_News_Cache_ClassCacheBuilder {
 	 * @param string $identifier identifier of the file
 	 * @return string path of the written file
 	 */
-	private static function writeFile($content, $identifier) {
-		if (!is_dir(PATH_site . 'typo3temp/Cache/Code/cache_phpcode/')) {
-			t3lib_div::mkdir_deep(PATH_site, 'typo3temp/Cache/Code/cache_phpcode/');
+	protected function writeFile($content, $identifier) {
+		$path = PATH_site . self::CACHE_FILE_LOCATION;
+		if (!is_dir($path)) {
+			t3lib_div::mkdir_deep(PATH_site, self::CACHE_FILE_LOCATION);
 		}
 
 		$content = '<?php ' . LF . $content . LF . '}' . LF . '?>';
 
-		$path = PATH_site . 'typo3temp/Cache/Code/cache_phpcode/' . str_replace('/', '_', $identifier) . '.php';
+		$path .= $this->generateFileNameFromIdentifier($identifier);
 
 		$success = t3lib_div::writeFile($path, $content);
 		if (!$success) {
-			throw new Exception ('file "' . $path . '" could not be written');
+			throw new RuntimeException('File "' . $path . '" could not be written');
 		}
 		return $path;
+	}
+
+	/**
+	 * Generate cache file name
+	 *
+	 * @param string $identifier identifier
+	 * @return string
+	 */
+	protected function generateFileNameFromIdentifier($identifier) {
+		if (!is_string($identifier) || empty($identifier)) {
+			throw new InvalidArgumentException('Given identifier is either not a string or empty');
+		}
+
+		$result = str_replace('/', '_', $identifier) . '.php';
+		$result = ucfirst($result);
+
+		return $result;
 	}
 
 	/**
@@ -123,19 +159,34 @@ class Tx_News_Cache_ClassCacheBuilder {
 	 * @param boolean $removeClassDefinition If class definition should be removed
 	 * @return string path of the saved file
 	 * @throws Exception
+	 * @throws InvalidArgumentException
 	 */
-	public static function parseSingleFile($filePath, $removeClassDefinition = TRUE) {
+	public function parseSingleFile($filePath, $removeClassDefinition = TRUE) {
 		if (!is_file($filePath)) {
-			throw new Exception(sprintf('File "%s" could not be found', $filePath));
+			throw new InvalidArgumentException(sprintf('File "%s" could not be found', $filePath));
 		}
 		$code = t3lib_div::getUrl($filePath);
-		if (empty($code)) {
-			throw new Exception(sprintf('File "%s" could not be fetched or is empty', $filePath));
-		}
-		$code = str_replace(array('<?php', '?>'), '', $code);
+		return $this->changeCode($code, $filePath, $removeClassDefinition);
+	}
 
-			// Remove everything before 'class Tx_', including namespaces,
-			// comments and require-statements.
+	/**
+	 * @param string $code
+	 * @param string $filePath
+	 * @param boolean $removeClassDefinition
+	 * @param boolean $renderPartialInfo
+	 * @return string
+	 * @throws Exception
+	 */
+	protected function changeCode($code, $filePath, $removeClassDefinition = TRUE, $renderPartialInfo = TRUE) {
+		if (empty($code)) {
+			throw new InvalidArgumentException(sprintf('File "%s" could not be fetched or is empty', $filePath));
+		}
+		$code = trim($code);
+		$code = str_replace(array('<?php', '?>'), '', $code);
+		$code = trim($code);
+
+		// Remove everything before 'class Tx_', including namespaces,
+		// comments and require-statements.
 		if ($removeClassDefinition) {
 			$pos = strpos($code, 'class Tx_');
 			$pos2 = strpos($code, '{', $pos);
@@ -143,15 +194,23 @@ class Tx_News_Cache_ClassCacheBuilder {
 			$code = substr($code, $pos2 + 1);
 		}
 
-			// Add some information for each partial
-		$code =  LF . LF . chr(10) . chr(10) . '/*' . str_repeat('*', 70) . LF . TAB .
-					'this is partial from: ' . $filePath . LF . str_repeat('*', 70) . '*/' . LF . $code;
+		$code = trim($code);
 
-			// Remove last }
+		// Add some information for each partial
+		if ($renderPartialInfo) {
+			$code = $this->getPartialInfo($filePath) . $code;
+		}
+
+		// Remove last }
 		$pos = strrpos($code, '}');
 		$code = substr($code, 0, $pos);
 		$code = trim($code);
 		return $code;
+	}
+
+	protected function getPartialInfo($filePath) {
+		return LF . LF . '/*' . str_repeat('*', 70) . LF . TAB .
+			'this is partial from: ' . $filePath . LF . str_repeat('*', 70) . '*/' . LF;
 	}
 
 }
---
+++ Tests/Unit/Cache/ClassCacheBuilderTest.php
@@ -0,0 +1,163 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *  (c) 2013 Georg Ringer <typo3@ringerge.org>
+ *  All rights reserved
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Tests for Tx_News_Tests_Unit_Cache_ClassCacheBuilder
+ *
+ * @package TYPO3
+ * @subpackage tx_news
+ * @author Georg Ringer <typo3@ringerge.org>
+ */
+class Tx_News_Tests_Unit_Cache_ClassCacheBuilderTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
+
+	/**
+	 * @test
+	 */
+	public function cacheFileCanBeWritten() {
+		$classCacheBuilder = $this->getAccessibleMock('Tx_News_Cache_ClassCacheBuilder', array('dummy'));
+		$content = '// this is some test';
+		$identifier = 'Test/Unit/Fo';
+
+		$writtenCacheFile = PATH_site . 'typo3temp/Cache/Code/cache_phpcode/' . $classCacheBuilder->_call('generateFileNameFromIdentifier', $identifier);
+
+		$classCacheBuilder->_call('writeFile', $content, $identifier);
+
+		$cacheFileContent = '<?php ' . LF . $content . LF . '}' . LF . '?>';
+		$this->assertEquals($cacheFileContent, t3lib_div::getUrl($writtenCacheFile));
+		unlink($writtenCacheFile);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider generatedCacheFileNameIsCorrectDataProvider
+	 */
+	public function generatedCacheFileNameIsCorrect($expectedResult, $given) {
+		$classCacheBuilder = $this->getAccessibleMock('Tx_News_Cache_ClassCacheBuilder', array('dummy'));
+		$result = $classCacheBuilder->_call('generateFileNameFromIdentifier', $given);
+
+		$this->assertEquals($expectedResult, $result, 'exp:' . $expectedResult . ' - ' . $result);
+	}
+
+	/**
+	 * Data provider
+	 *
+	 * @return array
+	 */
+	public function generatedCacheFileNameIsCorrectDataProvider() {
+		return array(
+			'expectedExample' => array('Fo_Bar_Blub.php', 'Fo/Bar/Blub'),
+			'simpleExample' => array('Fo.php', 'Fo'),
+			'simpleExampleAllLowerCase' => array('Fo.php', 'fo'),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider generatedCacheFileNameThrowsAnExceptionIfIdentifierIsWrongDataProvider
+	 * @expectedException InvalidArgumentException
+	 */
+	public function generatedCacheFileNameThrowsAnExceptionIfIdentifierIsWrong($given) {
+		$classCacheBuilder = $this->getAccessibleMock('Tx_News_Cache_ClassCacheBuilder', array('dummy'));
+		$classCacheBuilder->_call('generateFileNameFromIdentifier', $given);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function generatedCacheFileNameThrowsAnExceptionIfIdentifierIsWrongDataProvider() {
+		return array(
+			'emptyStringGiven' => array(''),
+			'NullGiven' => array(NULL),
+			'ObjectGiven' => array(new SplObjectStorage()),
+		);
+	}
+
+	/**
+	 * @test
+	 * @expectedException InvalidArgumentException
+	 */
+	public function parseSingleFileThrowsAnExceptionIfFileNotFound() {
+		$classCacheBuilder = $this->getAccessibleMock('Tx_News_Cache_ClassCacheBuilder', array('dummy'));
+		$classCacheBuilder->_call('parseSingleFile', 'someNonExistingDirectory/file.php');
+	}
+
+	/**
+	 * @test
+	 * @expectedException InvalidArgumentException
+	 */
+	public function changeCodeThrowsAnExceptionIfEmptyCodeGiven() {
+		$classCacheBuilder = $this->getAccessibleMock('Tx_News_Cache_ClassCacheBuilder', array('dummy'));
+		$classCacheBuilder->_call('changeCode', '', 'anyFile');
+	}
+
+	/**
+	 * @test
+	 * @dataProvider changeCodeWorksDataProvider
+	 */
+	public function changeCodeWorks($given, $expectedResult, $removeClassDefinition = TRUE) {
+		$classCacheBuilder = $this->getAccessibleMock('Tx_News_Cache_ClassCacheBuilder', array('dummy'));
+		$result = $classCacheBuilder->_call('changeCode', $given, 'SomeFile', $removeClassDefinition, FALSE);
+
+		$this->assertEquals($result, $expectedResult);
+	}
+
+	public function changeCodeWorksDataProvider() {
+		return array(
+			'BasicFile' => array(
+						$this->getClassExampleFileContent('BasicFile.php'),
+						$this->getClassExampleFileContent('BasicFileResult.txt')
+			),
+			'BasicFileWithInitialComment' => array(
+						$this->getClassExampleFileContent('BasicFileWithInitialComment.php'),
+						$this->getClassExampleFileContent('BasicFileResult.txt')
+			),
+			'BasicFileWithNamespace' => array(
+						$this->getClassExampleFileContent('BasicFileWithNamespace.php'),
+						$this->getClassExampleFileContent('BasicFileResult.txt')
+			),
+			'BasicFileAndKeepClassDefinition' => array(
+				$this->getClassExampleFileContent('BasicFileWithInitialComment.php'),
+				$this->getClassExampleFileContent('BasicFileWithClassDefinitionResult.txt'),
+				FALSE
+			),
+		);
+	}
+
+	/**
+	 * Return content of a example file.
+	 * Trimming is used as whitespaces can be ignored in unit tests
+	 * and should not lead to errors
+	 *
+	 * @param string $file
+	 * @return string
+	 */
+	protected function getClassExampleFileContent($file) {
+		$exampleFileDir = t3lib_extMgm::extPath('news') . 'Tests/Unit/Cache/ClassExamples/';
+
+		$content = t3lib_div::getUrl($exampleFileDir . $file);
+		$content = trim($content);
+		$content = str_replace("\r", '', $content);
+		$content = str_replace("\n", '', $content);
+
+		return $content;
+	}
+
+}
+
+?>
