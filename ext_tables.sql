509aaf189434cc5c424f56b27cec23ca1ac98092
Georg Ringer <georg.ringer@gmail.com>
2013-04-19 07:49:46 +0200

    [BUGFIX] Add crdate to cache table
    Change-Id: Ibe09970ad6371d0d6c79ed9d0c0f60f63967199b
    Resolves: #47293

--- ext_tables.sql
+++ ext_tables.sql
@@ -265,6 +265,7 @@ CREATE TABLE cf_news_categorycache (
     id int(11) unsigned NOT NULL auto_increment,
     identifier varchar(250) DEFAULT '' NOT NULL,
     expires int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate int(11) unsigned DEFAULT '0' NOT NULL,
     content mediumblob,
     lifetime int(11) unsigned DEFAULT '0' NOT NULL,
     PRIMARY KEY (id),
