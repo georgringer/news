1fe09f857731e212e2b7cac06e1c0f024883e67d
Georg Ringer <georg.ringer@gmail.com>
2013-05-04 10:51:30 +0200

    [!!!][TASK] PHP 5.3 is required
    PHP 5.3. is required
    
    Resolves: #47673
    Change-Id: I96035fa16290422a3de74a90259529c6d86bceee

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,6 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.5-6.1.99',
+			'php' => '5.3.0-0.0.0',
 			'extbase' => '',
 			'fluid' => '',
 		),
