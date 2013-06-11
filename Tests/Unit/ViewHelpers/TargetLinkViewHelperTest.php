de92ab2e8e281d6887e3944dd23d40a33a4f3df1
Georg Ringer <georg.ringer@gmail.com>
2013-06-11 13:45:21 +0200

    [FEATURE] Add VH for link target
    Change-Id: If7bc4e8f71f2d6014d0f018dd91ca1e8e6dd0d2e
    Resolves: #48868
    Reviewed-on: https://review.typo3.org/21335
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

---
+++ Tests/Unit/ViewHelpers/TargetLinkViewHelperTest.php
@@ -0,0 +1,102 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2013 Georg Ringer <typo3@ringerge.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Test for Tx_News_ViewHelpers_TargetLinkViewHelper
+ */
+class Tx_News_Tests_Unit_ViewHelpers_TargetLinkViewHelperTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
+
+	/**
+	 * @var $objectManager Tx_Extbase_Object_ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @param $objectManager Tx_Extbase_Object_ObjectManagerInterface
+	 * @return void
+	 */
+	protected function injectObjectManager(Tx_Extbase_Object_ObjectManagerInterface $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
+	 * @return Tx_News_ViewHelpers_TargetLinkViewHelper
+	 * @support
+	 */
+	protected function getPreparedInstance() {
+		$objectManager = t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager');
+		$instance = $objectManager->get('Tx_News_ViewHelpers_TargetLinkViewHelper');
+		return $instance;
+	}
+
+	/**
+	 * @test
+	 */
+	public function canCreateViewHelperClassInstance() {
+		$instance = $this->getPreparedInstance();
+		$this->assertInstanceOf('Tx_News_ViewHelpers_TargetLinkViewHelper', $instance);
+	}
+
+
+	/**
+	 * Test if correct target is returned
+	 *
+	 * @test
+	 * @dataProvider correctTargetIsReturnedDataProvider
+	 * @return void
+	 */
+	public function correctTargetIsReturned($link, $expectedResult) {
+		$viewHelper = new Tx_News_ViewHelpers_TargetLinkViewHelper();
+		$this->assertEquals($viewHelper->render($link), $expectedResult);
+	}
+
+	/**
+	 * Data provider
+	 *
+	 * @return array
+	 */
+	public function correctTargetIsReturnedDataProvider() {
+		return array(
+			'noTargetSetAndUrlDefined' => array(
+				'www.typo3.org', ''
+			),
+			'noTargetSetAndIdDefined' => array(
+				'123', ''
+			),
+			'IdAndTargetDefined' => array(
+				'123 _blank', '_blank'
+			),
+			'UrlAndPopupDefined' => array(
+				'www.typo3.org 300x400', ''
+			),
+			'ComplexExample' => array(
+				'www.typo3.org _fo my-class', '_fo'
+			),
+
+		);
+	}
+
+}
+
+?>
