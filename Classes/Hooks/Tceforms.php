1c4160b06926e07946c001ea0ee82c9027a23ab1
Georg Ringer <georg.ringer@gmail.com>
2013-08-23 06:20:23 +0200

    [BUGFIX] Fix predefined archive date for new records
    The archive date was only set if the record got an uid which is not
    needed, the pid is the only thing needed
    
    Change-Id: Ic4dc05d068cf13465e58b0727e7a0b0a83a940e2
    Resolves: #51247

--- Classes/Hooks/Tceforms.php
+++ Classes/Hooks/Tceforms.php
@@ -45,9 +45,8 @@ class Tx_News_Hooks_Tceforms {
 	 */
 	public function getSingleField_preProcess($table, $field, array &$row, $altName, $palette, $extra, $pal, t3lib_TCEforms $parentObject) {
 			// Predefine the archive date
-		if ($table === 'tx_news_domain_model_news' && is_numeric($row['uid'])) {
+		if ($table === 'tx_news_domain_model_news' && empty($row['archive']) && is_numeric($row['pid'])) {
 			$pagesTsConfig = t3lib_BEfunc::getPagesTSconfig($row['pid']);
-
 			if (is_array($pagesTsConfig['tx_news.']['predefine.'])
 					&& is_array($pagesTsConfig['tx_news.']['predefine.'])
 					&& isset($pagesTsConfig['tx_news.']['predefine.']['archive'])) {
