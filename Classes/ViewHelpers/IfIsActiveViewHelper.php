32ba01e98d8fd0b0a2e0611e319b89a53802aec5
Georg Ringer <georg.ringer@gmail.com>
2014-01-13 09:35:54 +0100

    [FEATURE] VH to check if current item is active
    The VH can be used if the same page shows a list view and a detail
    view. By using the VH the list view can have e.g. a special class to
    highlight the active one.
    
    Change-Id: I66690f71884b464e5b11237cff4641fbff7f89de
    Resolves: #54507
    Releases: master,4x
    Reviewed-on: https://review.typo3.org/26771
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

---
+++ Classes/ViewHelpers/IfIsActiveViewHelper.php
@@ -0,0 +1,50 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Georg Ringer <typo3@ringerge.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * ViewHelper to check if the current news item is rendered as single view on the same page
+ *
+ * # Example: Basic example
+ * <code>
+ * {n:ifIsActive(newsItem: newsItem, then: 'active', else: '')}
+ * </code>
+ * <output>
+ * Renders the string "active" if the current news item is active
+ * </output>
+ *
+ */
+class Tx_News_ViewHelpers_IfIsActiveViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractConditionViewHelper {
+
+	public function render(Tx_News_Domain_Model_News $newsItem) {
+		$vars = t3lib_div::_GET('tx_news_pi1');
+
+		if (isset($vars['news']) && (int)$newsItem->getUid() === (int)$vars['news']) {
+			return $this->renderThenChild();
+		} else {
+			return $this->renderElseChild();
+		}
+	}
+}
+
+?>
---
+++ Tests/Unit/ViewHelpers/IfIsActiveViewHelperTest.php
@@ -0,0 +1,84 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Georg Ringer <typo3@ringerge.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Tests for Tx_News_ViewHelpers_IfIsActiveViewHelper
+ */
+class Tx_News_Tests_Unit_ViewHelpers_IfIsActiveViewHelperTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
+
+	/**
+	 * @var Tx_News_ViewHelpers_IfIsActiveViewHelper
+	 */
+	protected $viewHelper;
+
+	public function setUp() {
+		parent::setUp();
+		$this->viewHelper = $this->getAccessibleMock('Tx_News_ViewHelpers_IfIsActiveViewHelper', array('renderThenChild', 'renderElseChild'));
+	}
+
+	/**
+	 * @test
+	 * @return void
+	 */
+	public function elseChildIsCalledWithNoGetArguments() {
+		$newsItem = new Tx_News_Domain_Model_News();
+		$newsItem->_setProperty('uid', 123);
+
+		$this->viewHelper->expects($this->once())
+			->method('renderElseChild');
+
+		$this->viewHelper->render($newsItem);
+	}
+
+	/**
+	 * @test
+	 * @return void
+	 */
+	public function elseChildIsCalledWithWrongGetArguments() {
+		$_GET['tx_news_pi1']['news'] = 456;
+		$newsItem = new Tx_News_Domain_Model_News();
+		$newsItem->_setProperty('uid', 123);
+
+		$this->viewHelper->expects($this->once())
+			->method('renderElseChild');
+
+		$this->viewHelper->render($newsItem);
+	}
+
+		/**
+	 * @test
+	 * @return void
+	 */
+	public function thenChildIsCalledWithCorrectArguments() {
+		$_GET['tx_news_pi1']['news'] = '789';
+		$newsItem = new Tx_News_Domain_Model_News();
+		$newsItem->_setProperty('uid', 789);
+
+		$this->viewHelper->expects($this->once())
+			->method('renderThenChild');
+
+		$this->viewHelper->render($newsItem);
+	}
+
+}
