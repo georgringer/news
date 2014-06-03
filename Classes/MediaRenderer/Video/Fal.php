698a99ee10d0a80a6fbbfd9f3204c6965b12353d
Georg Ringer <georg.ringer@gmail.com>
2014-06-03 14:11:09 +0200

    [SECURIT][BUGFIX] Update 3rd party libs
    Change-Id: Ia37b59e0aa75f9a707c8f8b1c17da633b74e2168

--- Classes/MediaRenderer/Video/Fal.php
+++ Classes/MediaRenderer/Video/Fal.php
@@ -49,7 +49,7 @@ class Tx_News_MediaRenderer_Video_Fal implements Tx_News_MediaRenderer_FalMediaI
 			$view->setTemplatePathAndFilename($templateFile);
 		}
 
-		$GLOBALS['TSFE']->getPageRenderer()->addJsFile(self::PATH_TO_JS . 'flowplayer-3.2.4.min.js');
+		$GLOBALS['TSFE']->getPageRenderer()->addJsFile(self::PATH_TO_JS . 'flowplayer-3.2.12.min.js');
 
 			// override width & height if both are set
 		if ($element->getWidth() > 0 && $element->getHeight() > 0) {
