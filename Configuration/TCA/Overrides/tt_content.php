3e2efc45f6694e18cb2aed7be9dcbae08c825d42
Georg Ringer <georg.ringer@gmail.com>
2014-11-28 10:03:15 +0100

    [BUGFIX] Bring back tt_content layout field
    Change-Id: I3b800bfa1b4eb90ef654d772fcf0b797db8d722b
    Resolves: #61096
    Reviewed-on: http://review.typo3.org/34729
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -15,7 +15,7 @@ $pluginSignature = strtolower($extensionName) . '_pi1';
 		'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xlf:pi1_title'
 );
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,recursive,select_key,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'recursive,select_key,pages';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_news.xml');
 
