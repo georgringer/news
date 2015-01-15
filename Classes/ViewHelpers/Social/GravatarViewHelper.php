a2cc81395e2a8e552280403c580c97a6796fe6ca
Josef Florian Glatz <jousch@gmail.com>
2015-01-15 09:34:44 +0100

    [FEATURE] Add default image argument to Gravatar ViewHelper
    Make it possible to show alternative image when
    an email address has no matching gravatar image.
    
    Change-Id: I5ce8c2391d17a4a1d1bc394223a7bb4ad36e40fc
    Resolves: #64291
    Reviewed-on: http://review.typo3.org/36020
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- Classes/ViewHelpers/Social/GravatarViewHelper.php
+++ Classes/ViewHelpers/Social/GravatarViewHelper.php
@@ -48,6 +48,7 @@ class Tx_News_ViewHelpers_Social_GravatarViewHelper extends \TYPO3\CMS\Fluid\Cor
 		$this->registerArgument('email', 'string', 'e-mail address of the user');
 		$this->registerArgument('size', 'integer', 'size since the images are square');
 		$this->registerArgument('alt', 'string', 'alt-text');
+		$this->registerArgument('default', 'string', 'default gravatar image (404, mm, identicon, monsterid, wavatar, retro, or blank)');
 	}
 
 	/**
@@ -56,9 +57,23 @@ class Tx_News_ViewHelpers_Social_GravatarViewHelper extends \TYPO3\CMS\Fluid\Cor
 	 * @return string
 	 */
 	public function render() {
-		$size = ((int) $this->arguments['size'] > 0) ? '?s=' . (int) $this->arguments['size'] : '';
-		$this->tag->addAttribute('src', self::GRAVATAR_IMAGE_REQUEST_URL . md5($this->arguments['email']) . $size);
+		$args = array();
+		if ((int) $this->arguments['size'] > 0) {
+			$args['s'] = (int) $this->arguments['size'];
+		}
+
+		if ($this->arguments['default']) {
+			$args['d'] = $this->arguments['default'];
+		}
+
+		if (count($args) > 0) {
+			$urlArgs = \TYPO3\CMS\Core\Utility\GeneralUtility::implodeArrayForUrl('', $args);
+			$urlArgs = '?' . ltrim($urlArgs, '&');
+		}
+
+		$this->tag->addAttribute('src', self::GRAVATAR_IMAGE_REQUEST_URL . md5($this->arguments['email']) . $urlArgs);
 		$this->tag->addAttribute('alt', $this->arguments['alt']);
+
 		return $this->tag->render();
 	}
 }
