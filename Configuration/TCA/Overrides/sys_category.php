3ceb56ff2b4695dc0e0f33913748d16cb2e07a0e
Georg Ringer <georg.ringer@gmail.com>
2015-02-06 08:13:06 +0100

    [TASK] Improve image selection in category
    Change-Id: Ib8ab6f4c1bc7da776045389009f509be2d7d3551
    Reviewed-on: http://review.typo3.org/36704
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- Configuration/TCA/Overrides/sys_category.php
+++ Configuration/TCA/Overrides/sys_category.php
@@ -67,8 +67,12 @@ $newSysCategoryColumns = array(
 			'images',
 			array(
 				'appearance' => array(
-					'createNewRelationLinkTitle' => 'LLL:EXT:cms/locallang_ttc.xlf:images.addFileReference'
-				),
+					'createNewRelationLinkTitle' => 'LLL:EXT:cms/locallang_ttc.xlf:images.addFileReference',
+					'showPossibleLocalizationRecords' => 1,
+					'showRemovedLocalizationRecords' => 1,
+					'showAllLocalizationLink' => 1,
+					'showSynchronizationLink' => 1
+					),
 				'foreign_match_fields' => array(
 					'fieldname' => 'images',
 					'tablenames' => 'sys_category',
