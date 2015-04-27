5c14bfcb6ce0734b80281ea207535abe1a73efcd
Georg Ringer <georg.ringer@gmail.com>
2015-04-27 10:24:33 +0200

    [BUGFIX] Prevent loosing saved categories if some are restricted
    Change-Id: I498f0fe1bf2b16f5b552642c77f6068cdb82c913
    Reviewed-on: http://review.typo3.org/38947
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- Classes/Hooks/Tcemain.php
+++ Classes/Hooks/Tcemain.php
@@ -2,7 +2,7 @@
 
 namespace GeorgRinger\News\Hooks;
 
-	/**
+/**
  * This file is part of the TYPO3 CMS project.
  *
  * It is free software; you can redistribute it and/or modify it under
@@ -16,6 +16,7 @@ namespace GeorgRinger\News\Hooks;
  */
 use GeorgRinger\News\Service\AccessControlService;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Hook into tcemain which is used to show preview of news item
@@ -27,32 +28,33 @@ class Tcemain {
 
 	/**
 	 * Flushes the cache if a news record was edited.
-     * This happens on two levels: by UID and by PID.
+	 * This happens on two levels: by UID and by PID.
 	 *
 	 * @param array $params
 	 * @return void
 	 */
 	public function clearCachePostProc(array $params) {
 		if (isset($params['table']) && $params['table'] === 'tx_news_domain_model_news') {
-            $cacheTagsToFlush = array();
-            if (isset($params['uid'])) {
-                $cacheTagsToFlush[] = 'tx_news_uid_' . $params['uid'];
-            }
-            if (isset($params['uid_page'])) {
-                $cacheTagsToFlush[] = 'tx_news_pid_' . $params['uid_page'];
-            }
+			$cacheTagsToFlush = array();
+			if (isset($params['uid'])) {
+				$cacheTagsToFlush[] = 'tx_news_uid_' . $params['uid'];
+			}
+			if (isset($params['uid_page'])) {
+				$cacheTagsToFlush[] = 'tx_news_pid_' . $params['uid_page'];
+			}
 
-            /** @var $cacheManager \TYPO3\CMS\Core\Cache\CacheManager */
-            $cacheManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager');
-            foreach ($cacheTagsToFlush as $cacheTag) {
-                $cacheManager->getCache('cache_pages')->flushByTag($cacheTag);
-                $cacheManager->getCache('cache_pagesection')->flushByTag($cacheTag);
-            }
-        }
-    }
+			/** @var $cacheManager \TYPO3\CMS\Core\Cache\CacheManager */
+			$cacheManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager');
+			foreach ($cacheTagsToFlush as $cacheTag) {
+				$cacheManager->getCache('cache_pages')->flushByTag($cacheTag);
+				$cacheManager->getCache('cache_pagesection')->flushByTag($cacheTag);
+			}
+		}
+	}
 
 	/**
 	 * Generate a different preview link     *
+	 *
 	 * @param string $status status
 	 * @param string $table table name
 	 * @param integer $recordUid id of the record
@@ -64,7 +66,7 @@ class Tcemain {
 		// Clear category cache
 		if ($table === 'sys_category') {
 			/** @var \TYPO3\CMS\Core\Cache\Frontend\FrontendInterface $cache */
-			$cache = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager')->getCache('cache_news_category');
+			$cache = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Cache\\CacheManager')->getCache('cache_news_category');
 			$cache->flush();
 		}
 
@@ -95,7 +97,7 @@ class Tcemain {
 						$parameters['L'] = $record['sys_language_uid'];
 					}
 
-					$GLOBALS['_POST']['popViewId_addParams'] = \TYPO3\CMS\Core\Utility\GeneralUtility::implodeArrayForUrl('', $parameters, '', FALSE, TRUE);
+					$GLOBALS['_POST']['popViewId_addParams'] = GeneralUtility::implodeArrayForUrl('', $parameters, '', FALSE, TRUE);
 					$GLOBALS['_POST']['popViewId'] = $pagesTsConfig['tx_news.']['singlePid'];
 				}
 			}
@@ -103,7 +105,7 @@ class Tcemain {
 	}
 
 	/**
-	 * Prevent saving of a news record if the editor doesn't have access to all categories of the news recird
+	 * Prevent saving of a news record if the editor doesn't have access to all categories of the news record
 	 *
 	 * @param array $fieldArray
 	 * @param string $table
@@ -113,19 +115,34 @@ class Tcemain {
 	public function processDatamap_preProcessFieldArray(&$fieldArray, $table, $id, $parentObject) {
 		if ($table === 'tx_news_domain_model_news') {
 			// check permissions of assigned categories
-			if (is_int($id) && !$GLOBALS['BE_USER']->isAdmin()) {
+			if (is_int($id) && !$this->getBackendUser()->isAdmin()) {
 				$newsRecord = BackendUtility::getRecord($table, $id);
 				if (!AccessControlService::userHasCategoryPermissionsForRecord($newsRecord)) {
 					$parentObject->log($table, $id, 2, 0, 1, "processDatamap: Attempt to modify a record from table '%s' without permission. Reason: the record has one or more categories assigned that are not defined in your BE usergroup.", 1, array($table));
 					// unset fieldArray to prevent saving of the record
 					$fieldArray = array();
+				} else {
+
+					// If the category relation has been modified, no | is found anymore
+					if (strpos($fieldArray['categories'], '|') === FALSE) {
+
+						$deniedCategories = AccessControlService::getAccessDeniedCategories($newsRecord);
+						if (is_array($deniedCategories)) {
+							foreach($deniedCategories as $deniedCategory) {
+								$fieldArray['categories'] .= ',' . $deniedCategory['uid'];
+							}
+							$fieldArray['categories'] = trim($fieldArray['categories'], ',');
+						}
+					}
+
 				}
+
 			}
 		}
 	}
 
 	/**
-	 * Prevent deleting/moving of a news record if the editor doesn't have access to all categories of the news recird
+	 * Prevent deleting/moving of a news record if the editor doesn't have access to all categories of the news record
 	 *
 	 * @param string $command
 	 * @param string $table
@@ -134,7 +151,7 @@ class Tcemain {
 	 * @param $parentObject \TYPO3\CMS\Core\DataHandling\DataHandler
 	 */
 	public function processCmdmap_preProcess($command, &$table, $id, $value, $parentObject) {
-		if ($table === 'tx_news_domain_model_news' && !$GLOBALS['BE_USER']->isAdmin() && is_integer($id)) {
+		if ($table === 'tx_news_domain_model_news' && !$this->getBackendUser()->isAdmin() && is_integer($id)) {
 			$newsRecord = BackendUtility::getRecord($table, $id);
 			if (!AccessControlService::userHasCategoryPermissionsForRecord($newsRecord)) {
 				$parentObject->log($table, $id, 2, 0, 1, "processCmdmap: Attempt to " . $command . " a record from table '%s' without permission. Reason: the record has one or more categories assigned that are not defined in the BE usergroup.", 1, array($table));
@@ -142,6 +159,22 @@ class Tcemain {
 				$table = '';
 			}
 		}
+	}
+
+	/**
+	 * Returns the current BE user.
+	 *
+	 * @return \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
+	 */
+	protected function getBackendUser() {
+		return $GLOBALS['BE_USER'];
+	}
+
+	/**
+	 * @return \TYPO3\Cms\Core\Database\DatabaseConnection
+	 */
+	protected static function getDatabaseConnection() {
+		return $GLOBALS['TYPO3_DB'];
 	}
 
 }
