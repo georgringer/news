8131b97c48fc244601f77faf3b46ebe7628ea4f4
Georg Ringer <georg.ringer@gmail.com>
2015-09-30 06:02:01 +0200

    [TASK] Set minimum reqired version to 7.5
    Change-Id: I6926f0249addd0d3d34d8d87c9692568f0c72715
    Reviewed-on: http://review.typo3.org/43639
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "news".
- *
- * Auto generated 12-02-2014 21:00
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'News system',
 	'description' => 'Versatile news extension, based on extbase & fluid. Editor friendly, default integration of social sharing and many other features',
@@ -31,13 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '3.2.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.0.0-7.99.99',
+			'typo3' => '7.5.0-7.99.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
 	),
-	'_md5_values_when_last_written' => '',
 	'suggests' => array(),
 );
-
-?>
