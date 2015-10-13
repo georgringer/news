2c78182ae1babb94c087f6431e8ef6034aa76f28
Georg Ringer <georg.ringer@gmail.com>
2015-10-13 05:09:30 +0200

    [BUGFIX] Fix edit links in administration module
    Change-Id: Icddef86e6c6b057ca0a82df33c695c5e8b9c7704
    Resolves: #70430
    Reviewed-on: http://review.typo3.org/44017
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

---
+++ Classes/ViewHelpers/Be/EditRecordViewHelper.php
@@ -0,0 +1,63 @@
+<?php
+namespace GeorgRinger\News\ViewHelpers\Be;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\FormProtection\FormProtectionFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
+
+/**
+ * Edit Record ViewHelper, see FormEngine logic
+ */
+class EditRecordViewHelper extends AbstractViewHelper implements CompilableInterface
+{
+    /**
+     * Returns a URL to link to FormEngine
+     *
+     * @param string $parameters Is a set of GET params to send to FormEngine
+     * @return string URL to FormEngine module + parameters
+     * @see \TYPO3\CMS\Backend\Utility\BackendUtility::getModuleUrl()
+     */
+    public function render($parameters)
+    {
+        return static::renderStatic(
+            array(
+                'parameters' => $parameters
+            ),
+            $this->buildRenderChildrenClosure(),
+            $this->renderingContext
+        );
+    }
+
+    /**
+     * @param array $arguments
+     * @param callable $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     *
+     * @return string
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        $parameters = GeneralUtility::explodeUrl2Array($arguments['parameters']);
+
+        $parameters['returnUrl'] = 'index.php?M=web_NewsTxNewsM2&id=' . (int)GeneralUtility::_GET('id')
+            . '&moduleToken=' . FormProtectionFactory::get()->generateToken('moduleCall', 'web_NewsTxNewsM2');
+
+        return BackendUtility::getModuleUrl('record_edit', $parameters);
+    }
+}
