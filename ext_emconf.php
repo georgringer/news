ea2159e8281b03d9b8668e85fa7cb2a6f3b23c7d
Georg Ringer <g.ringer@supseven.at>
2016-04-15 07:29:05 +0200

    [TASK] Make it compatible with core < 8.3
    It works in 8, so write it down.
    
    Change-Id: I6937b4f01787dd5494dc1021057388b6cbe24f35
    Reviewed-on: https://review.typo3.org/47684
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '4.2.1',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '7.6.0-7.99.99',
+			'typo3' => '7.6.0-8.2.99',
 		],
 		'conflicts' => [],
 		'suggests' => [
