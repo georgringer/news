8d19a01d40c72d5b2282b171a3f497b98694c4fa
Georg Ringer <georg.ringer@gmail.com>
2016-07-26 21:18:41 +0200

    [FEATURE] Native support for EXT:rx_shariff
    If the extension "rx_shariff" is laoded, it is used instead of the custom social viewhelpers
    
    Change-Id: I25a4b4059ddeb9c1a894505110d39acfcf56beaa
    Reviewed-on: https://review.typo3.org/49219
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

---
+++ Classes/ViewHelpers/ExtensionLoadedViewHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace GeorgRinger\News\ViewHelpers;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
+
+/**
+ * Class ExtensionLoadedViewHelper
+ */
+class ExtensionLoadedViewHelper extends AbstractConditionViewHelper
+{
+
+    /**
+     * Initialize additional argument
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('extensionKey', 'string', 'Extension which must be checked', true);
+        parent::initializeArguments();
+    }
+
+    /**
+     * @param array|null $arguments
+     * @return bool
+     */
+    protected static function evaluateCondition($arguments = null)
+    {
+        return ExtensionManagementUtility::isLoaded($arguments['extensionKey']);
+    }
+}
