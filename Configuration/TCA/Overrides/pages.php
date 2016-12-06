a431f395542c729e566f4dbf7a2e72afe43567fe
Georg Ringer <georg.ringer@gmail.com>
2016-12-06 23:16:54 +0100

    [TASK] Move call to registerPageTSConfigFile to TCA/Overrides
    To improve the performance, the call to registerPageTSConfigFile
    should be moved from the ext_tables,php to the TCA/Overrides.
    
    Related: #181

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -7,3 +7,8 @@ $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
     1 => 'news',
     2 => 'apps-pagetree-folder-contains-news'
 ];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'news',
+    'Configuration/TSconfig/Page/news_only.txt',
+    'EXT:news :: Restrict pages to news records');
