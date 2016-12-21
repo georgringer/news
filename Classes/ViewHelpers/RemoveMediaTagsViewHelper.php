fa8340c39b0850c0c0f7ba0d3f7a6ba89c14c5f3
Johannes <schugabe@gmx.at>
2016-12-21 13:36:36 +0100

    [FEATURE] add ViewHelper to display media files inline (#167)
    [media] tags in teaser/bodytext are replaced with attached media files
    
    Resolves #120

---
+++ Classes/ViewHelpers/RemoveMediaTagsViewHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GeorgRinger\News\ViewHelpers;
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class RemoveMediaTagsViewHelper extends AbstractViewHelper
+{
+    protected $escapingInterceptorEnabled = false;
+
+    protected $escapeOutput = false;
+
+    protected $escapeChildren = false;
+
+    /**
+     * @var string[]
+     */
+    protected $tags = ['[media]'];
+
+    /**
+     * @return mixed
+     */
+    public function render() {
+
+        $content = $this->renderChildren();
+        return str_replace($this->tags, '', $content);
+    }
+}
