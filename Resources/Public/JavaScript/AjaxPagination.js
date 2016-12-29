572fee8c4a149be14729db13d47c8b7880bbf425
Georg Ringer <georg.ringer@gmail.com>
2016-12-29 20:23:41 +0100

    [FEATURE] Document the AJAX pagination
    Resolves: #161
    Change-Id: Icd9ea2abcee50107ab961dea294da9c07ee18a78

--- Resources/Public/JavaScript/AjaxPagination.js
+++ Resources/Public/JavaScript/AjaxPagination.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
     $('.news').on('click', '.page-navigation a', function (e) {
-        e.preventDefault();
         var ajaxUrl = $(this).data('link');
-        var container = 'news-container-' + $(this).data('container');
-        $.ajax({
-            url: ajaxUrl,
-            type: 'GET',
-            success: function (result) {
-                var ajaxDom = $(result).find('#' + container);
-                $('#' + container).replaceWith(ajaxDom);
-            }
-        });
+        if (ajaxUrl !== undefined && ajaxUrl !== '') {
+            e.preventDefault();
+            var container = 'news-container-' + $(this).data('container');
+            $.ajax({
+                url: ajaxUrl,
+                type: 'GET',
+                success: function (result) {
+                    var ajaxDom = $(result).find('#' + container);
+                    $('#' + container).replaceWith(ajaxDom);
+                }
+            });
+        }
     });
 });
