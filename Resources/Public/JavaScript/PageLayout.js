5f75a6a659b188f14a2e1384c4b4d4b221079dbb
Georg Ringer <georg.ringer@gmail.com>
2017-01-31 19:44:00 +0100

    [FEATURE] Make plugin overview nice on small columns
    Improve the CSS and use a bit JS to make the overview
    nice if there is not so much place.
    
    Resolves: #241

--- Resources/Public/JavaScript/PageLayout.js
+++ Resources/Public/JavaScript/PageLayout.js
@@ -1,5 +1,15 @@
 define(['jquery'], function ($) {
 
+    var table = $('.news-table');
+
+
+
+    $(table).each(function() {
+        if ($(this).width() < 350) {
+            $(this).addClass('news-table-small');
+        }
+    });
+
     $('.news-table tfoot a').click(function (e, element) {
         $(this).toggleClass('open');
         $('#' + $(this).data('identifier')).toggleClass('hidden');
