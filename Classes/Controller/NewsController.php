64d4cc60885dbcca9340cc94abf89b2d946498ba
Georg Ringer <georg.ringer@gmail.com>
2017-04-11 11:19:55 +0200

    [BUGFIX] Add caseinsensitive security check for overwriteDemandObject
    Change-Id: I3b5e5579f7b65c5ea8c9357031e9cce0667cccd8

--- Classes/Controller/NewsController.php
+++ Classes/Controller/NewsController.php
@@ -41,7 +41,7 @@ class NewsController extends NewsBaseController
     protected $configurationManager;
 
     /** @var array */
-    protected $ignoredSettingsForOverride = ['demandClass', 'orderByAllowed'];
+    protected $ignoredSettingsForOverride = ['demandclass', 'orderbyallowed'];
 
     /**
      * Inject a news repository to enable DI
@@ -148,6 +148,9 @@ class NewsController extends NewsBaseController
         }
 
         foreach ($overwriteDemand as $propertyName => $propertyValue) {
+            if(in_array(strtolower($propertyName), $this->ignoredSettingsForOverride, true)) {
+                continue;
+            }
             if ($propertyValue !== '' || $this->settings['allowEmptyStringsForOverwriteDemand']) {
                 \TYPO3\CMS\Extbase\Reflection\ObjectAccess::setProperty($demand, $propertyName, $propertyValue);
             }
