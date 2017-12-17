bcb666e55f86983f6074c98273cfb64c7c1a380d
Georg Ringer <georg.ringer@gmail.com>
2017-12-17 13:00:39 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- Classes/Hooks/Backend/RecordListQueryHook8.php
+++ Classes/Hooks/Backend/RecordListQueryHook8.php
@@ -14,7 +14,6 @@ use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Messaging\FlashMessageService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Lang\LanguageService;
-use TYPO3\CMS\Recordlist\RecordList\AbstractDatabaseRecordList;
 
 /**
  * Hook into DatabaseRecordList to hide tt_content elements in list view
