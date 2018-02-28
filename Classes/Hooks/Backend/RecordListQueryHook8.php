0a9b19dc55b8de25965b948c0628faf10f734205
Georg Ringer <georg.ringer@gmail.com>
2018-02-28 09:00:23 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- Classes/Hooks/Backend/RecordListQueryHook8.php
+++ Classes/Hooks/Backend/RecordListQueryHook8.php
@@ -39,7 +39,6 @@ class RecordListQueryHook8
                                 array $fieldList,
                                 QueryBuilder $queryBuilder)
     {
-
         if ($table === 'tt_content' && $pageId > 0) {
             $pageRecord = BackendUtility::getRecord('pages', $pageId, 'uid', ' AND doktype="254" AND module="news"');
             if (is_array($pageRecord)) {
@@ -72,8 +71,7 @@ class RecordListQueryHook8
         array $fieldList,
         $parentObject,
         $queryBuilder = null
-    )
-    {
+    ) {
         if ($table === 'tt_content' && (int)$parentObject->searchLevels === 0 && $parentObject->id > 0) {
             $pageRecord = BackendUtility::getRecord('pages', $parentObject->id, 'uid', ' AND doktype="254" AND module="news"');
             if (is_array($pageRecord)) {
