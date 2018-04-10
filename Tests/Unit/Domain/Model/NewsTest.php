2838c6567f4cfebfa891af1ec6bd537dd2f2f472
Georg Ringer <georg.ringer@gmail.com>
2018-04-10 07:42:32 +0200

    [BUGFIX] Fix failing test

--- Tests/Unit/Domain/Model/NewsTest.php
+++ Tests/Unit/Domain/Model/NewsTest.php
@@ -315,7 +315,7 @@ class NewsTest extends UnitTestCase
 
         $mockedElement1 = $this->getAccessibleMock('GeorgRinger\\News\\Domain\\Model\\FileReference', ['getProperty']);
         $mockedElement1->_set('uid', 1);
-        $mockedElement1->_set('showinpreview', true);
+        $mockedElement1->_set('showinpreview', 1);
         $mockedElement1->expects($this->any())->method('getProperty')->will($this->returnValue(true));
 
         $mediaItem1 = new FileReference();
@@ -324,7 +324,7 @@ class NewsTest extends UnitTestCase
 
         $mockedElement2 = $this->getAccessibleMock('GeorgRinger\\News\\Domain\\Model\\FileReference', ['getProperty']);
         $mockedElement2->_set('uid', 2);
-        $mockedElement2->_set('showinpreview', true);
+        $mockedElement2->_set('showinpreview', 1);
         $mockedElement2->expects($this->any())->method('getProperty')->will($this->returnValue(false));
 
         $mediaItem2 = new FileReference();
@@ -333,16 +333,14 @@ class NewsTest extends UnitTestCase
 
         $mockedElement3 = $this->getAccessibleMock('GeorgRinger\\News\\Domain\\Model\\FileReference', ['getProperty']);
         $mockedElement3->_set('uid', 3);
-        $mockedElement3->_set('showinpreview', true);
+        $mockedElement3->_set('showinpreview', 1);
         $mockedElement3->expects($this->any())->method('getProperty')->will($this->returnValue(true));
 
         $mediaItem3 = new FileReference();
         $mediaItem3->_setProperty('originalResource', $mockedElement3);
         $news->addFalMedia($mediaItem3);
 
-        $this->assertEquals(2, count($news->getFalMediaPreviews()));
         $this->assertEquals(2, count($news->getMediaPreviews()));
-        $this->assertEquals(1, count($news->getFalMediaNonPreviews()));
         $this->assertEquals(1, count($news->getMediaNonPreviews()));
         $this->assertEquals(3, count($news->getFalMedia()));
         $this->assertEquals(3, count($news->getMedia()));
