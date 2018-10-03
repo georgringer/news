e949ec5187139fa60bf19389934c7943ded6906f
Esteban Marin <estebanmarin@gmx.ch>
2018-10-03 18:38:40 +0200

    allow typo3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '7.0.5',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.13-9.4.99',
+            'typo3' => '8.7.13-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [
