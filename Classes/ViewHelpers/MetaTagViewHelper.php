48f8b2597044f2bc1b40e6ba3957b04510bea428
Florian Wessels <f.wessels@bitmotion.de>
2019-05-09 15:14:28 +0200

    [BUGFIX] Use proper method for setting meta tags

--- Classes/ViewHelpers/MetaTagViewHelper.php
+++ Classes/ViewHelpers/MetaTagViewHelper.php
@@ -89,7 +89,12 @@ class MetaTagViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBase
 
         if ($useCurrentDomain || (isset($this->arguments['content']) && !empty($this->arguments['content']))) {
             $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
-            $pageRenderer->addMetaTag($this->tag->render());
+
+            if ($this->tag->hasAttribute('property')) {
+                $pageRenderer->setMetaTag('property', $this->tag->getAttribute('property'), $this->tag->getAttribute('content'));
+            } elseif ($this->tag->hasAttribute('name')) {
+                $pageRenderer->setMetaTag('property', $this->tag->getAttribute('name'), $this->tag->getAttribute('content'));
+            }
         }
     }
 }
