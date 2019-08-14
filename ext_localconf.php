ebad625e8d58a147cc3a05fa029178b994cb3665
Georg Ringer <georg.ringer@gmail.com>
2019-08-14 06:21:36 +0200

    [TASK] Add isRegistered check for custom icons
    Only register icons if those are not yet registered already
    
    Resolves: #958

--- ext_localconf.php
+++ ext_localconf.php
@@ -142,11 +142,13 @@ $boot = function () {
         ];
         $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
         foreach ($icons as $identifier => $path) {
-            $iconRegistry->registerIcon(
-                $identifier,
-                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-                ['source' => 'EXT:news/Resources/Public/Icons/' . $path]
-            );
+            if (!$iconRegistry->isRegistered($identifier)) {
+                $iconRegistry->registerIcon(
+                    $identifier,
+                    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                    ['source' => 'EXT:news/Resources/Public/Icons/' . $path]
+                );
+            }
         }
     }
 
