125c99c1f61fbd8b730a80fc9f0b7dc995928b8b
Georg Ringer <georg.ringer@gmail.com>
2019-08-15 16:43:03 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- Classes/Hooks/DataHandler.php
+++ Classes/Hooks/DataHandler.php
@@ -8,7 +8,6 @@ namespace GeorgRinger\News\Hooks;
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  */
-
 use GeorgRinger\News\Backend\NewsSlugHelper;
 use GeorgRinger\News\Service\AccessControlService;
 use TYPO3\CMS\Backend\Utility\BackendUtility as BackendUtilityCore;
