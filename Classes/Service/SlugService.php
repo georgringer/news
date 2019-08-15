8e04323a3f14d7e0bc77c3542b47a3d2a42d0616
Georg Ringer <georg.ringer@gmail.com>
2019-08-15 18:42:34 +0200

    [TASK] Use proper check

--- Classes/Service/SlugService.php
+++ Classes/Service/SlugService.php
@@ -24,7 +24,7 @@ class SlugService
 
     public function __construct()
     {
-        if (version_compare(TYPO3_branch, '9.5', '<=')) {
+        if (version_compare(TYPO3_branch, '9.5', '<')) {
             $this->slugService = GeneralUtility::makeInstance(NewsSlugHelper::class);
         } else {
             $fieldConfig = $GLOBALS['TCA']['tx_news_domain_model_news']['columns']['path_segment']['config'];
