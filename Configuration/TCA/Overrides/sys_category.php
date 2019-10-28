b88c4c9d1f0665719c4c2750d9b62721f4a70b11
in-session <info@in-session.de>
2019-10-28 13:40:16 +0100

    Bug if sysfolder of categories not in PseudoSite
    https://forge.typo3.org/issues/89400

--- Configuration/TCA/Overrides/sys_category.php
+++ Configuration/TCA/Overrides/sys_category.php
@@ -144,7 +144,6 @@ if (version_compare(TYPO3_branch, '9.5', '>=')) {
     $newSysCategoryColumns['slug'] = [
         'exclude' => true,
         'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_tca.xlf:pages.slug',
-        'displayCond' => 'USER:' . \TYPO3\CMS\Core\Compatibility\PseudoSiteTcaDisplayCondition::class . '->isInPseudoSite:pages:false',
         'config' => [
             'type' => 'slug',
             'size' => 50,
