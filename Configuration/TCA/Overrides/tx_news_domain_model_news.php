9fd89417297d8de081b9c7bc22e589c5882a7367
Georg Ringer <georg.ringer@gmail.com>
2020-05-05 13:51:58 +0200

    [TASK] Avoid TCA migration entries for 10 LTS

---
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -0,0 +1,14 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+$boot = static function () {
+    // Remove TCA settings for version 10 to avoid entries in TCA migration check
+    if(\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class)->getMajorVersion() === 10) {
+        foreach(['link', 'news', 'tag'] as $tableSuffix) {
+            unset($GLOBALS['TCA']['tx_news_domain_model_' . $tableSuffix]['interface']['showRecordFieldList']);
+        }
+    }
+};
+
+$boot();
+unset($boot);
