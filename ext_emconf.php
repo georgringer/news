2a3bf518b9a69f26496aacd0cb8997ddc68cdf3a
Georg Ringer <georg.ringer@gmail.com>
2020-05-12 13:38:13 +0200

    [TASK] Raise requirements for TYPO3 core
    Latest bugfix release 9.5.17 or 10.4.2 is required as the
    slug eval handling has been fixed there.

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '8.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.16-10.9.99',
+            'typo3' => '9.5.17-10.9.99',
         ],
         'conflicts' => [],
         'suggests' => [
