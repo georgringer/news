f00c3575c543cc31b6ae01a0de6dbc630f03a3a6
Georg Ringer <georg.ringer@gmail.com>
2020-09-30 14:55:04 +0200

    [BUGFIX] Use correct return param

--- Classes/Hooks/FilterMenuPagesHook.php
+++ Classes/Hooks/FilterMenuPagesHook.php
@@ -21,7 +21,7 @@ use TYPO3\CMS\Frontend\ContentObject\Menu\AbstractMenuFilterPagesHookInterface;
 class FilterMenuPagesHook implements AbstractMenuFilterPagesHookInterface
 {
     /**
-     * Returns true if the page should be shown
+     * Returns true if the page should be hidden
      * @param array $data
      * @param array $banUidArray
      * @param bool $spacer
@@ -31,13 +31,13 @@ class FilterMenuPagesHook implements AbstractMenuFilterPagesHookInterface
     public function processFilter(array &$data, array $banUidArray, $spacer, AbstractMenuContentObject $obj)
     {
         if (!isset($data['_PAGES_OVERLAY_REQUESTEDLANGUAGE'])) {
-            return true;
+            return false;
         }
         try {
             $availability = GeneralUtility::makeInstance(NewsAvailability::class);
             return $availability->check($data['_PAGES_OVERLAY_REQUESTEDLANGUAGE']);
         } catch (\Exception $e) {
-            return true;
+            return false;
         }
     }
 
