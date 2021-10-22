d5e3941fa1a6ad99d8aa3f2ce7d9e70dfca80282
Georg Ringer <georg.ringer@gmail.com>
2021-10-22 10:00:33 +0200

    [TASK] Allow rx_shariff v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
         'conflicts' => [],
         'suggests' => [
-            'rx_shariff' => '12.0.0-13.99.99',
+            'rx_shariff' => '12.0.0-14.99.99',
             'numbered_pagination' => '1.0.1-1.99.99'
         ],
     ],
