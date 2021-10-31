81a850f77bad421d75e7cf5eae1b4e71b8d2f3a9
Igor Neyman <igorneyman82@gmail.com>
2021-10-31 14:32:14 +0000

    [BUGFIX] Fixing php warnings

--- Classes/Hooks/Backend/RecordListQueryHook.php
+++ Classes/Hooks/Backend/RecordListQueryHook.php
@@ -51,7 +51,8 @@ class RecordListQueryHook
             $pageRecord = BackendUtility::getRecord('pages', $pageId, 'uid', " AND doktype='254' AND module='news'");
             if (is_array($pageRecord)) {
                 $tsConfig = BackendUtility::getPagesTSconfig($pageId);
-                if (isset($tsConfig['tx_news.']) && is_array($tsConfig['tx_news.']) && $tsConfig['tx_news.']['showContentElementsInNewsSysFolder'] == 1) {
+                if (isset($tsConfig['tx_news.']) && is_array($tsConfig['tx_news.']) && 
+                    isset($tsConfig['tx_news.']['showContentElementsInNewsSysFolder']) && $tsConfig['tx_news.']['showContentElementsInNewsSysFolder'] == 1) {
                     return;
                 }
 
