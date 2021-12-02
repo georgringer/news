9ab99cc3fd74537156f84687f47fd9c314f3711b
Georg Ringer <georgringer@users.noreply.github.com>
2021-12-02 19:49:51 +0100

    [BUGFIX] Replace outdated usage of clearable
    Resolves: #1661

--- Resources/Public/JavaScript/AdministrationModule.js
+++ Resources/Public/JavaScript/AdministrationModule.js
@@ -1,20 +1,19 @@
-define(['jquery', 'TYPO3/CMS/Backend/Tooltip'], function ($) {
+define(['jquery', 'TYPO3/CMS/Backend/Tooltip', 'TYPO3/CMS/Backend/Input/Clearable'], function ($) {
 
-    if ($('.t3js-clearable').length) {
-        require(['TYPO3/CMS/Backend/jquery.clearable'], function() {
-            $('.t3js-clearable').clearable();
-        });
+  var clearables = Array.from(document.querySelectorAll('.t3js-clearable')).filter(inputElement => {
+    // Filter input fields being a date time picker and a color picker
+    return !inputElement.classList.contains('t3js-datetimepicker') && !inputElement.classList.contains('t3js-color-picker');
+  });
+  clearables.forEach(clearableField => clearableField.clearable());
+  $(document).ready(function () {
+    var form = $('#administrationForm');
+    if (form.data('autosubmitform') == 1) {
+      form.submit();
     }
-
-    $(document).ready(function () {
-        var form = $('#administrationForm');
-        if (form.data('autosubmitform') == 1) {
-            form.submit();
-        }
-        $('a[data-togglelink="1"]').click(function (e) {
-            e.preventDefault();
-            $('#setting-container').toggle();
-        });
+    $('a[data-togglelink="1"]').click(function (e) {
+      e.preventDefault();
+      $('#setting-container').toggle();
     });
+  });
 
 });
