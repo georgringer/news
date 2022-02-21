811472ccc3ca8eaa4d33195daf78a3e67923f657
Georg Ringer <georgringer@users.noreply.github.com>
2022-02-21 14:51:57 +0100

    [TASK] Adopt sorting in LinkHandlerTargetPageService

--- Classes/Service/LinkHandlerTargetPageService.php
+++ Classes/Service/LinkHandlerTargetPageService.php
@@ -55,7 +55,7 @@ class LinkHandlerTargetPageService
             ->where(
                 $queryBuilder->expr()->eq('sys_category_record_mm.tablenames', $queryBuilder->createNamedParameter('tx_news_domain_model_news', \PDO::PARAM_STR)),
                 $queryBuilder->expr()->gt('sys_category.single_pid', $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT)),
-                $queryBuilder->expr()->eq('sys_category_record_mm.uid_foreign', $queryBuilder->createNamedParameter($newsId, \PDO::PARAM_INT))
+                $queryBuilder->expr()->eq('sys_category_record_mm.sorting_foreign', $queryBuilder->createNamedParameter($newsId, \PDO::PARAM_INT))
             )
             ->orderBy('sys_category_record_mm.sorting')
             ->setMaxResults(1)
