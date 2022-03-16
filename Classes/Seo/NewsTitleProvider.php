2df8606804fdb0db8ff479768b71ff50e4058863
Georg Ringer <georg.ringer@gmail.com>
2022-03-16 11:53:50 +0100

    [TASK] Adopt class copyright

--- Classes/Seo/NewsTitleProvider.php
+++ Classes/Seo/NewsTitleProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace GeorgRinger\News\Seo;
 
 /**
- * This file is part of the "tt_address" Extension for TYPO3 CMS.
+ * This file is part of the "news" Extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
