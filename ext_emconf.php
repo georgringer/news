8cdf6b7d24e77ebadae927cd178c08d2dbd10835
Georg Ringer <georg.ringer@gmail.com>
2022-10-12 20:20:09 +0200

    [TASK] Set correct version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '10.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.13-11.9.99',
+            'typo3' => '11.5.0-12.9.99',
         ],
         'conflicts' => [],
         'suggests' => [
