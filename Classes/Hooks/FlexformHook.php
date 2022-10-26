b2721f93cc55209411db818a85aaf20dd2fd9ef6
Georg Ringer <georg.ringer@gmail.com>
2022-10-26 09:55:47 +0200

    [TASK] 1st cleanup of FlexformHook

--- Classes/Hooks/FlexformHook.php
+++ Classes/Hooks/FlexformHook.php
@@ -15,103 +15,10 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Hook into \TYPO3\CMS\Backend\Utility\BackendUtility to change flexform behaviour
- * depending on action selection
+ * depending on view
  */
 class FlexformHook
 {
-
-    /**
-     * Fields which are removed in detail view
-     *
-     * @var array
-     */
-    public $removedFieldsInDetailView = [
-        'sDEF' => 'orderBy,orderDirection,categories,categoryConjunction,includeSubCategories,
-						archiveRestriction,timeRestriction,timeRestrictionHigh,topNewsRestriction,
-						dateField,selectedList',
-        'additional' => 'limit,offset,hidePagination,topNewsFirst,listPid,list.paginate.itemsPerPage',
-        'template' => 'cropMaxCharacters'
-    ];
-
-    /**
-     * Fields which are removed in list view
-     *
-     * @var array
-     */
-    public $removedFieldsInListView = [
-        'sDEF' => 'dateField,singleNews,previewHiddenRecords,selectedList',
-        'additional' => '',
-        'template' => ''
-    ];
-
-    /**
-     * Fields which are removed in selected list view
-     *
-     * @var array
-     */
-    public $removedFieldsInSelectedListView = [
-        'sDEF' => 'categories,categoryConjunction,includeSubCategories,
-						archiveRestriction,timeRestriction,timeRestrictionHigh,topNewsRestriction,
-						startingpoint,recursive,dateField,singleNews,previewHiddenRecords,
-                        previewHiddenRecords,startingpoint,recursive',
-        'additional' => 'tags,limit,offset,hidePagination,topNewsFirst,backPid,excludeAlreadyDisplayedNews,
-								list.paginate.itemsPerPage,disableOverrideDemand',
-        'template' => ''
-    ];
-
-    /**
-     * Fields which are removed in dateMenu view
-     *
-     * @var array
-     */
-    public $removedFieldsInDateMenuView = [
-        'sDEF' => 'orderBy,singleNews,selectedList',
-        'additional' => 'limit,offset,hidePagination,topNewsFirst,backPid,previewHiddenRecords,excludeAlreadyDisplayedNews,
-								list.paginate.itemsPerPage',
-        'template' => 'cropMaxCharacters,media.maxWidth,media.maxHeight'
-    ];
-
-    /**
-     * Fields which are removed in search form view
-     *
-     * @var array
-     */
-    public $removedFieldsInSearchFormView = [
-        'sDEF' => 'orderBy,orderDirection,categories,categoryConjunction,includeSubCategories,
-						archiveRestriction,timeRestriction,timeRestrictionHigh,topNewsRestriction,
-						startingpoint,recursive,dateField,singleNews,previewHiddenRecords,selectedList',
-        'additional' => 'limit,offset,hidePagination,topNewsFirst,detailPid,backPid,excludeAlreadyDisplayedNews,
-								list.paginate.itemsPerPage',
-        'template' => 'cropMaxCharacters,media.maxWidth,media.maxHeight'
-    ];
-
-    /**
-     * Fields which are removed in category list view
-     *
-     * @var array
-     */
-    public $removedFieldsInCategoryListView = [
-        'sDEF' => 'orderBy,orderDirection,categoryConjunction,includeSubCategories,
-						archiveRestriction,timeRestriction,timeRestrictionHigh,topNewsRestriction,
-						recursive,dateField,singleNews,previewHiddenRecords,selectedList',
-        'additional' => 'limit,offset,hidePagination,topNewsFirst,detailPid,backPid,excludeAlreadyDisplayedNews,
-								list.paginate.itemsPerPage',
-        'template' => 'cropMaxCharacters,media.maxWidth,media.maxHeight'
-    ];
-
-    /**
-     * Fields which are removed in tag list view
-     *
-     * @var array
-     */
-    public $removedFieldsInTagListView = [
-        'sDEF' => 'categories,categoryConjunction,includeSubCategories,
-						archiveRestriction,timeRestriction,timeRestrictionHigh,topNewsRestriction,
-						dateField,singleNews,previewHiddenRecords,selectedList',
-        'additional' => 'limit,offset,hidePagination,topNewsFirst,detailPid,backPid,excludeAlreadyDisplayedNews,
-								list.paginate.itemsPerPage',
-        'template' => 'cropMaxCharacters,media.maxWidth,media.maxHeight'
-    ];
 
     /** @var EmConfiguration */
     protected $configuration;
@@ -190,32 +97,6 @@ class FlexformHook
         }
 
         if (!empty($selectedView)) {
-            // Modify the flexform structure depending on the first found action
-            switch ($selectedView) {
-                case 'News->list':
-                case 'News->searchResult':
-                    $this->deleteFromStructure($dataStructure, $this->removedFieldsInListView);
-                    break;
-                case 'News->detail':
-                    $this->deleteFromStructure($dataStructure, $this->removedFieldsInDetailView);
-                    break;
-                case 'News->selectedList':
-                    $this->deleteFromStructure($dataStructure, $this->removedFieldsInSelectedListView);
-                    break;
-                case 'News->searchForm':
-                    $this->deleteFromStructure($dataStructure, $this->removedFieldsInSearchFormView);
-                    break;
-                case 'News->dateMenu':
-                    $this->deleteFromStructure($dataStructure, $this->removedFieldsInDateMenuView);
-                    break;
-                case 'Category->list':
-                    $this->deleteFromStructure($dataStructure, $this->removedFieldsInCategoryListView);
-                    break;
-                case 'Tag->list':
-                    $this->deleteFromStructure($dataStructure, $this->removedFieldsInTagListView);
-                    break;
-                default:
-            }
 
             if ($GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['Hooks/BackendUtility.php']['updateFlexforms'] ?? []) {
                 $params = [
@@ -227,65 +108,5 @@ class FlexformHook
                 }
             }
         }
-    }
-
-    /**
-     * Add category restriction to flexforms
-     *
-     * @param array $structure
-     *
-     * @return void
-     */
-    protected function addCategoryConstraints(&$structure): void
-    {
-        $categoryRestrictionSetting = $this->configuration->getCategoryRestriction();
-        $categoryRestriction = '';
-        switch ($categoryRestrictionSetting) {
-            case 'current_pid':
-                $categoryRestriction = ' AND sys_category.pid=###CURRENT_PID### ';
-                break;
-            case 'siteroot':
-                $categoryRestriction = ' AND sys_category.pid IN (###SITEROOT###) ';
-                break;
-            case 'page_tsconfig':
-                $categoryRestriction = ' AND sys_category.pid IN (###PAGE_TSCONFIG_IDLIST###) ';
-                break;
-        }
-
-        if (!empty($categoryRestriction) && isset($structure['sheets']['sDEF']['ROOT']['el']['settings.categories'])) {
-            $structure['sheets']['sDEF']['ROOT']['el']['settings.categories']['TCEforms']['config']['foreign_table_where'] = $categoryRestriction . $structure['sheets']['sDEF']['ROOT']['el']['settings.categories']['TCEforms']['config']['foreign_table_where'];
-        }
-    }
-
-    /**
-     * Remove fields from flexform structure
-     *
-     * @param array &$dataStructure flexform structure
-     * @param array $fieldsToBeRemoved fields which need to be removed
-     *
-     * @return void
-     */
-    protected function deleteFromStructure(array &$dataStructure, array $fieldsToBeRemoved): void
-    {
-        foreach ($fieldsToBeRemoved as $sheetName => $sheetFields) {
-            $fieldsInSheet = GeneralUtility::trimExplode(',', $sheetFields, true);
-
-            foreach ($fieldsInSheet as $fieldName) {
-                unset($dataStructure['sheets'][$sheetName]['ROOT']['el']['settings.' . $fieldName]);
-            }
-        }
-    }
-
-    /**
-     * @param int $pageId
-     * @return bool
-     */
-    protected function enabledInTsConfig($pageId): bool
-    {
-        $tsConfig = BackendUtilityCore::getPagesTSconfig($pageId);
-        if (isset($tsConfig['tx_news.']['categoryRestrictionForFlexForms'])) {
-            return (bool)$tsConfig['tx_news.']['categoryRestrictionForFlexForms'];
-        }
-        return false;
     }
 }
