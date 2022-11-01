b2c6cea14cf63729b215e587ec642c9573e6f479
Stefan Bürk <stefan.buerk@impactmedia.de>
2022-11-01 19:54:59 +0100

    [TASK] Avoid prophecy in skipped TagControllerTest
    This change adjusts the skipped TagControllerTest to avoid
    phpspec/prophecy, which eleminates the need to install it.
    
    A todo is added, to remember for decision how to proceed
    with this test.

--- Tests/Unit/Controller/TagControllerTest.php
+++ Tests/Unit/Controller/TagControllerTest.php
@@ -12,40 +12,28 @@ use GeorgRinger\News\Controller\TagController;
 use GeorgRinger\News\Domain\Model\Dto\NewsDemand;
 use GeorgRinger\News\Domain\Repository\TagRepository;
 use TYPO3\CMS\Fluid\View\TemplateView;
-use TYPO3\TestingFramework\Core\BaseTestCase;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 /**
  * Testcase for the TagController class.
  *
  *
  */
-class TagControllerTest extends BaseTestCase
+class TagControllerTest extends UnitTestCase
 {
-    private $tagRepository = null;
-
     /**
-     * Set up framework
-     *
-     */
-    public function setup(): void
-    {
-        $this->tagRepository = $this->prophesize(TagRepository::class);
-    }
-
-    /**
-     * Test for creating correct demand call
-     *
      * @test
-     *
-     * @return void
+     * @todo Eventually migrate this to a functional test instead, remove it or get it working again.
      */
     public function listActionFindsDemandedTagsByDemandFromSettings(): void
     {
         $this->markTestSkipped('May not be relevant anymore. Reason: failing because of using DI');
+        $tagRepositoryMock = $this->getMockBuilder(TagRepository::class)->disableOriginalConstructor()->getMock();
         $demand = new NewsDemand();
         $settings = ['list' => 'foo', 'orderBy' => 'datetime'];
 
-        $fixture->_set('tagRepository', $this->tagRepository->reveal());
+        $fixture = $this->getAccessibleMock(TagController::class);
+        $fixture->_set('tagRepository', $tagRepositoryMock);
         $fixture->setView($this->getMockBuilder(TemplateView::class)->disableOriginalConstructor()->getMock());
         $fixture->_set('settings', $settings);
 
