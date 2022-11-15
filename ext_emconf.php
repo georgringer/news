f52b8468232ea147ef4bce6f7968636be719a361
Georg Ringer <georg.ringer@gmail.com>
2022-11-15 11:24:06 +0100

    [TASK] Force 11.5.19 to avoid regression of 11.5.18

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '10.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.18-12.9.99',
+            'typo3' => '11.5.19-12.9.99',
         ],
         'conflicts' => [],
         'suggests' => [
