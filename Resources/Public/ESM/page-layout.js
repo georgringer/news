6b6d7662e97304efe166c38b0e2e96bfc58ea2ee
Benjamin Franzke <bfr@qbus.de>
2023-08-04 21:22:08 +0200

    [TASK] Add support for TYPO3 v12 ES6 modules (#2168)
    RequireJS has been deprecated in TYPO3 v12. [1]
    
    Migrate existing backend javascript modules
    to ECMAScript6 and configure a respective
    import map. [2]
    
    TYPO3 v11 compatibility is preserved by keep
    using RequireJS.
    
    [1] https://docs.typo3.org/c/typo3/cms-core/12.4/en-us/Changelog/12.0/Deprecation-97057-DeprecateRequireJSSupport.html
    [2] https://docs.typo3.org/c/typo3/cms-core/12.4/en-us/Changelog/12.0/Feature-96510-InfrastructureForJavaScriptModulesAndImportmaps.html

---
+++ Resources/Public/ESM/page-layout.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+var table = $('.news-table');
+
+$(table).each(function() {
+    if ($(this).width() < 350) {
+        $(this).addClass('news-table-small');
+    }
+});
+
+$('.news-table tfoot a').click(function (e, element) {
+    $(this).toggleClass('open');
+    $('#' + $(this).data('identifier')).toggleClass('hidden');
+    e.preventDefault();
+});
