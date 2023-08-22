87b4bee5095565d9193219227f0691ec94f5f57e
Georg Ringer <georgringer@users.noreply.github.com>
2023-08-22 08:11:43 +0200

    [BUGFIX] Fix notice in InlineElementHook

--- Classes/Hooks/InlineElementHook.php
+++ Classes/Hooks/InlineElementHook.php
@@ -71,7 +71,7 @@ class InlineElementHook implements InlineElementHookInterface
         $isVirtual,
         array &$controlItems
     ) {
-        $previewSetting = (int)(is_array($childRecord['showinpreview'] ?? false) ? $childRecord['showinpreview'][0] : ($childRecord['showinpreview'] ?? 0));
+        $previewSetting = (int)(is_array($childRecord['showinpreview'] ?? false) ?? $childRecord['showinpreview'][0] ?? $childRecord['showinpreview'] ?? 0);
         if ($foreignTable === 'sys_file_reference' && $previewSetting > 0) {
             $ll = 'LLL:EXT:news/Resources/Private/Language/locallang_db.xlf:';
 
