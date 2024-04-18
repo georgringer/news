1a31e75508af23f39db5f3c558bebd7689a3bb98
Georg Ringer <georg.ringer@gmail.com>
2024-04-18 20:52:05 +0200

    [TASK] Cleanup AddNewsToMenuProcessor
    Remove usage of GU::_GET

--- Classes/DataProcessing/AddNewsToMenuProcessor.php
+++ Classes/DataProcessing/AddNewsToMenuProcessor.php
@@ -16,6 +16,7 @@ use TYPO3\CMS\Core\Context\Exception\AspectNotFoundException;
 use TYPO3\CMS\Core\Context\LanguageAspect;
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Routing\PageArguments;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
@@ -74,11 +75,9 @@ class AddNewsToMenuProcessor implements DataProcessorInterface
      */
     protected function getNewsRecord(): array
     {
-        $newsId = 0;
-        $vars = GeneralUtility::_GET('tx_news_pi1');
-        if (isset($vars['news'])) {
-            $newsId = (int)$vars['news'];
-        }
+        /** @var PageArguments $routing */
+        $routing = $GLOBALS['TYPO3_REQUEST']->getAttribute('routing');
+        $newsId = (int)($routing->getArguments()['tx_news_pi1']['news'] ?? 0);
 
         if ($newsId) {
             $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
