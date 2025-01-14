616eabf4e0a8c11a79c19bb8f098b1b86cec2ed7
J.Kummer <typo3@enobe.de>
2025-01-14 10:18:18 +0100

    [TASK] Remove RealURL migration (#2598)

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use GeorgRinger\News\Hooks\ItemsProcFunc;
 use GeorgRinger\News\Hooks\PluginPreviewRenderer;
 use GeorgRinger\News\Updates\NewsSlugUpdater;
-use GeorgRinger\News\Updates\RealurlAliasNewsSlugUpdater;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use TYPO3\CMS\Core\DependencyInjection\SingletonPass;
@@ -14,12 +13,10 @@ return function (ContainerConfigurator $container, ContainerBuilder $containerBu
     $containerBuilder->registerForAutoconfiguration(ItemsProcFunc::class)->addTag('news.ItemsProcFunc');
     $containerBuilder->registerForAutoconfiguration(PluginPreviewRenderer::class)->addTag('news.PageLayoutView');
     $containerBuilder->registerForAutoconfiguration(NewsSlugUpdater::class)->addTag('news.NewsSlugUpdater');
-    $containerBuilder->registerForAutoconfiguration(RealurlAliasNewsSlugUpdater::class)->addTag('news.RealurlAliasNewsSlugUpdater');
 
     $containerBuilder->addCompilerPass(new SingletonPass('news.NewsFlexFormManipulation'));
     $containerBuilder->addCompilerPass(new SingletonPass('news.RecordListQueryHook'));
     $containerBuilder->addCompilerPass(new SingletonPass('news.ItemsProcFunc'));
     $containerBuilder->addCompilerPass(new SingletonPass('news.PageLayoutView'));
     $containerBuilder->addCompilerPass(new SingletonPass('news.NewsSlugUpdater'));
-    $containerBuilder->addCompilerPass(new SingletonPass('news.RealurlAliasNewsSlugUpdater'));
 };
