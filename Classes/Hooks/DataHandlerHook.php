f0e94c88da5b07bd5a2cc566ca322484fd08617b
Markus Klein <markus.klein@typo3.org>
2025-07-08 23:32:14 +0200

    [BUGFIX] Reset cacheTagsToFlush array after flush operation (#2656)

--- Classes/Hooks/DataHandlerHook.php
+++ Classes/Hooks/DataHandlerHook.php
@@ -42,6 +42,7 @@ class DataHandlerHook implements SingletonInterface
             foreach (array_unique($this->cacheTagsToFlush) as $cacheTag) {
                 $cacheManager->flushCachesInGroupByTag('pages', $cacheTag);
             }
+            $this->cacheTagsToFlush = [];
         }
     }
 
