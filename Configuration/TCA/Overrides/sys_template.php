45ef57b80132f89b2880d64577649240b50b910f
Georg Ringer <typo3@ringer.it>
2025-10-18 09:19:37 +0200

    [!!!][TASK] Remove deprecated sitemap option
    Remove the old sitemap TypoScript which used cObj CONTENT.
    Switch to EXT:seo and its sitemap feature which is supported in
    EXT:news since the beginning

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -5,7 +5,6 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 defined('TYPO3') or die;
 
 ExtensionManagementUtility::addStaticFile('news', 'Configuration/TypoScript', 'News');
-ExtensionManagementUtility::addStaticFile('news', 'Configuration/TypoScript/Sitemap', 'News Sitemap (deprecated)');
 ExtensionManagementUtility::addStaticFile('news', 'Configuration/TypoScript/SeoSitemap', 'News Sitemap (tx_seo)');
 ExtensionManagementUtility::addStaticFile('news', 'Configuration/TypoScript/Styles/Twb', 'News Styles Twitter Bootstrap');
 ExtensionManagementUtility::addStaticFile('news', 'Configuration/TypoScript/Styles/Twb5', 'News Styles Twitter Bootstrap V5');
