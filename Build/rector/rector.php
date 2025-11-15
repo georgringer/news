34e87ed3491c46af171dc31d85071703443df8f9
Georg Ringer <georgringer@users.noreply.github.com>
2025-11-15 22:47:59 +0100

    [TASK] Fix PHP version in ext_emconf

--- Build/rector/rector.php
+++ Build/rector/rector.php
@@ -41,7 +41,7 @@ return RectorConfig::configure()
     ])
     ->withImportNames(true, true, false, true)
     ->withConfiguredRule(ExtEmConfRector::class, [
-        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.2.0-8.4.99',
+        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.2.0-8.5.99',
         ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '13.4.20-14.0.0',
         ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
     ])
