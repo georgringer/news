d43ae0cd0d698709c87b0a8703c68739f305625f
Georg Ringer <typo3@ringer.it>
2026-05-18 22:44:23 +0200

    [BUGFIX] Add missing quote

--- Classes/Domain/Repository/NewsRepository.php
+++ Classes/Domain/Repository/NewsRepository.php
@@ -350,6 +350,8 @@ class NewsRepository extends AbstractDemandedRepository
      */
     public function countByDate(DemandInterface $demand): array
     {
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionForTable('tx_news_domain_model_news');
         $data = [];
         $sql = $this->findDemandedRaw($demand);
 
@@ -358,10 +360,8 @@ class NewsRepository extends AbstractDemandedRepository
 
         // Get the month/year into the result
         $field = $demand->getDateField();
-        $field = empty($field) ? 'datetime' : $field;
+        $field = $connection->quoteIdentifier(empty($field) ? 'datetime' : $field);
 
-        $connection = GeneralUtility::makeInstance(ConnectionPool::class)
-            ->getConnectionForTable('tx_news_domain_model_news');
         $isPostgres = $connection->getDatabasePlatform() instanceof PostgreSQLPlatform;
         $isSqlite = $connection->getDatabasePlatform() instanceof SQLitePlatform;
         if ($isPostgres) {
