d503d6bd8665b9913195f46a014295f1755d50de
Georg Ringer <georgringer@users.noreply.github.com>
2026-08-07 00:12:30 +0200

    [BUGFIX] Adopt failing test

--- Tests/Unit/Hooks/PluginPreviewRendererTest.php
+++ Tests/Unit/Hooks/PluginPreviewRendererTest.php
@@ -13,6 +13,11 @@ use GeorgRinger\News\Hooks\PluginPreviewRenderer;
 use GeorgRinger\News\Utility\TemplateLayout;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\MockObject\MockObject;
+use TYPO3\CMS\Backend\Preview\StandardContentPreviewRenderer;
+use TYPO3\CMS\Backend\Routing\UriBuilder;
+use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\TestingFramework\Core\AccessibleObjectInterface;
 use TYPO3\TestingFramework\Core\BaseTestCase;
@@ -34,7 +39,21 @@ class PluginPreviewRendererTest extends BaseTestCase
 
         $GLOBALS['LANG'] = $languageService;
 
-        $this->pageLayoutView = $this->getAccessibleMock(PluginPreviewRenderer::class, ['getRecord'], [], '', false);
+        $icon = $this->createMock(Icon::class);
+        $icon->method('render')->willReturn('<icon/>');
+        $iconFactory = $this->createMock(IconFactory::class);
+        $iconFactory->method('getIcon')->willReturn($icon);
+
+        $this->pageLayoutView = $this->getAccessibleMock(
+            PluginPreviewRenderer::class,
+            ['getRecord'],
+            [
+                $iconFactory,
+                $this->createMock(UriBuilder::class),
+                $this->createMock(EventDispatcher::class),
+                $this->createMock(StandardContentPreviewRenderer::class),
+            ],
+        );
         $this->pageLayoutView->expects(self::any())->method('getRecord')->willReturn(null);
     }
 
